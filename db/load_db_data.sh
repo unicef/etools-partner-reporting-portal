d8a1f44deceeb4abb9330e5dffc12fc32f5f7563
Silin Na <n1207n@gmail.com>
2017-03-21 15:27:42 -0700

    feat: Created db PostGIS docker container

---
+++ db/load_db_data.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+# Perform all actions as $POSTGRES_USER
+export PGUSER="$POSTGRES_USER"
+
+# Create the 'template_postgis' template db
+"${psql[@]}" <<- 'EOSQL'
+  CREATE DATABASE template_postgis;
+  UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+EOSQL
+
+# Load PostGIS into both template_database and $POSTGRES_DB
+for DB in template_postgis "$POSTGRES_DB"; do
+	echo "Loading PostGIS extensions into $DB"
+	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
+		CREATE EXTENSION IF NOT EXISTS postgis;
+		CREATE EXTENSION IF NOT EXISTS postgis_topology;
+		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+EOSQL
+
+echo "*** CREATING DATABASE ***"
+
+# create default database
+"${psql[@]}" <<- 'EOSQL'
+    CREATE ROLE etools_prp_user WITH superuser login;
+    CREATE DATABASE etools_prp;
+    GRANT ALL PRIVILEGES ON DATABASE etools_prp TO etools_prp_user;
+EOSQL
+
+# Use this if there is a db dump file
+# echo "*** UPDATING DATABASE ***"
+# export DB_DUMP_LOCATION=/tmp/psql_data/db1.bz2
+#
+# bzcat $DB_DUMP_LOCATION | nice pg_restore --verbose  -U etools_prp_user -F t -d etools_prp
+
+echo "*** DATABASE CREATED! ***"
+
+done
