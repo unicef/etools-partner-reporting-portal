f2a00dc2d7c2171b20aa86ee9c1d85406ee6f48e
Silin Na <n1207n@gmail.com>
2017-03-23 15:42:18 -0700

    feat: Created few Django apps [#5]
    + Added *.pyc to gitignore
    
    + Added host directory volume mounting to docker-compose

--- django_api/django_api/settings/base.py
+++ django_api/django_api/settings/base.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+sys.path.append(os.path.join(BASE_DIR, 'apps/'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
