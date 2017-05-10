80f70a9b7d7c94bac47706e0e013975178e0f656
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-05-10 14:59:41 +0200

    add hotfix get_fullname funtion to User models

--- django_api/django_api/apps/account/models.py
+++ django_api/django_api/apps/account/models.py
@@ -5,11 +5,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    partner = models.ForeignKey('core.Partner', related_name="users")
+    partner = models.ForeignKey('core.Partner', related_name="users", null=True, blank=True)
     organization = models.CharField(max_length=255)
 
     def __str__(self):
         return "{} - User".format(self.get_fullname())
+
+    def get_fullname(self):
+        return self.username  # TODO: this should be improved
 
 
 class UserProfile(models.Model):
