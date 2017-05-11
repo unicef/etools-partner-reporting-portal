792bde2aa66d71220eb801b034214b9a6f176afb
Silin Na <n1207n@gmail.com>
2017-05-11 16:18:32 -0700

    feat: Created IndicatorListCreateAPIView with serializer and URL pattern [#19]
    + Overrode IndicatorListSerializer to include IndicatorBlueprint fields

--- django_api/django_api/apps/indicator/views.py
+++ django_api/django_api/apps/indicator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListCreateAPIView
+
+from .serializers import IndicatorListSerializer
+from .models import Reportable
+
+
+class IndicatorListCreateAPIView(ListCreateAPIView):
+    """
+    REST API endpoint to get a list of Indicator objects and to create a new Indicator object.
+    """
+    queryset = Reportable.objects.all()
+    serializer_class = IndicatorListSerializer
