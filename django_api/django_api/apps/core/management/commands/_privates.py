f99c9ce65ddf0cc8f951ac723894a216c3584eb5
Silin Na <n1207n@gmail.com>
2017-05-16 11:59:49 -0700

    refactor: Hid all logic to _privates and added optional argument for generate_fake_data [#50]
    + --clean_before

--- django_api/django_api/apps/core/management/commands/_privates.py
+++ django_api/django_api/apps/core/management/commands/_privates.py
@@ -21,11 +21,34 @@ from unicef.models import (
     LowerLevelOutput,
 )
 
+from core.factories import (
+    UserFactory,
+    UserProfileFactory,
+    ClusterFactory,
+    ClusterObjectiveFactory,
+    ClusterActivityFactory,
+    PartnerFactory,
+    PartnerProjectFactory,
+    PartnerActivityFactory,
+    IndicatorBlueprintFactory,
+    ReportableToIndicatorReportFactory,
+    ReportableToClusterActivityFactory,
+    ReportableToPartnerActivityFactory,
+    IndicatorDisaggregationFactory,
+    IndicatorDataSpecificationFactory,
+    IndicatorReportFactory,
+    ProgressReportFactory,
+    ProgrammeDocumentFactory,
+    CountryProgrammeOutputFactory,
+    LowerLevelOutputFactory,
+)
+
 
 def clean_up_data():
-    if settings['ENV'] == 'dev':
+    if settings.ENV == 'dev':
+        print "Deleting all ORM objects"
+
         User.objects.all().delete()
-        UserProfile.objects.all().delete()
         Cluster.objects.all().delete()
         ClusterObjective.objects.all().delete()
         ClusterActivity.objects.all().delete()
@@ -41,3 +64,60 @@ def clean_up_data():
         ProgrammeDocument.objects.all().delete()
         CountryProgrammeOutput.objects.all().delete()
         LowerLevelOutput.objects.all().delete()
+
+        print "All ORM objects deleted"
+
+def generate_fake_data(quantity=3):
+    UserFactory.create_batch(quantity)
+    print "{} User objects created".format(quantity)
+
+    ClusterFactory.create_batch(quantity)
+    print "{} Cluster objects created".format(quantity)
+
+    ClusterObjectiveFactory.create_batch(quantity)
+    print "{} ClusterObjective objects created".format(quantity)
+
+    ClusterActivityFactory.create_batch(quantity)
+    print "{} ClusterActivity objects created".format(quantity)
+
+    PartnerFactory.create_batch(quantity)
+    print "{} Partner objects created".format(quantity)
+
+    PartnerProjectFactory.create_batch(quantity)
+    print "{} PartnerProject objects created".format(quantity)
+
+    PartnerActivityFactory.create_batch(quantity)
+    print "{} PartnerActivity objects created".format(quantity)
+
+    IndicatorBlueprintFactory.create_batch(quantity)
+    print "{} IndicatorBlueprint objects created".format(quantity)
+
+    ReportableToIndicatorReportFactory.create_batch(quantity)
+    print "{} ReportableToIndicatorReport objects created".format(quantity)
+
+    ReportableToClusterActivityFactory.create_batch(quantity)
+    print "{} ReportableToClusterActivity objects created".format(quantity)
+
+    ReportableToPartnerActivityFactory.create_batch(quantity)
+    print "{} ReportableToPartnerActivity objects created".format(quantity)
+
+    IndicatorDisaggregationFactory.create_batch(quantity)
+    print "{} IndicatorDisaggregation objects created".format(quantity)
+
+    IndicatorDataSpecificationFactory.create_batch(quantity)
+    print "{} IndicatorDataSpecification objects created".format(quantity)
+
+    IndicatorReportFactory.create_batch(quantity)
+    print "{} IndicatorReport objects created".format(quantity)
+
+    ProgressReportFactory.create_batch(quantity)
+    print "{} ProgressReport objects created".format(quantity)
+
+    ProgrammeDocumentFactory.create_batch(quantity)
+    print "{} ProgrammeDocument objects created".format(quantity)
+
+    CountryProgrammeOutputFactory.create_batch(quantity)
+    print "{} CountryProgrammeOutput objects created".format(quantity)
+
+    LowerLevelOutputFactory.create_batch(quantity)
+    print "{} LowerLevelOutput objects created".format(quantity)
--- django_api/django_api/apps/core/management/commands/generate_fake_data.py
+++ django_api/django_api/apps/core/management/commands/generate_fake_data.py
@@ -1,54 +1,26 @@
 from django.core.management.base import BaseCommand
 
-from _privates import clean_up_data
-
-from core.factories import (
-    UserFactory,
-    UserProfileFactory,
-    ClusterFactory,
-    ClusterObjectiveFactory,
-    ClusterActivityFactory,
-    PartnerFactory,
-    PartnerProjectFactory,
-    PartnerActivityFactory,
-    IndicatorBlueprintFactory,
-    ReportableFactory,
-    IndicatorDisaggregationFactory,
-    IndicatorDataSpecificationFactory,
-    IndicatorReportFactory,
-    ProgressReportFactory,
-    ProgrammeDocumentFactory,
-    CountryProgrammeOutputFactory,
-    LowerLevelOutputFactory,
-)
+from _privates import clean_up_data, generate_fake_data
 
 
 class Command(BaseCommand):
     help = 'Creates a set of ORM objects for initial data'
 
     def add_arguments(self, parser):
-        parser.add_arguments('clean_before', nargs='+', type=bool)
+        parser.add_argument(
+            '--clean_before',
+            action='store_true',
+            dest='clean_before',
+            default=False,
+            help='Clean up all ORM objects before generating fake data'
+        )
 
     def handle(self, *args, **options):
-        if 'clean_before' in options:
+        quantity = 3
+
+        if options['clean_before']:
             clean_up_data()
 
-        UserFactory.create_batch(3)
-        UserProfileFactory.create_batch(3)
-        ClusterFactory.create_batch(3)
-        ClusterObjectiveFactory.create_batch(3)
-        ClusterActivityFactory.create_batch(3)
-        PartnerFactory.create_batch(3)
-        PartnerProjectFactory.create_batch(3)
-        PartnerActivityFactory.create_batch(3)
-        IndicatorBlueprintFactory.create_batch(3)
-        ReportableToIndicatorReportFactory.create_batch(3)
-        ReportableToClusterActivityFactory.create_batch(3)
-        ReportableToPartnerActivityFactory.create_batch(3)
-        IndicatorDisaggregationFactory.create_batch(3)
-        IndicatorDataSpecificationFactory.create_batch(3)
-        IndicatorReportFactory.create_batch(3)
-        ProgressReportFactory.create_batch(3)
-        ProgrammeDocumentFactory.create_batch(3)
-        CountryProgrammeOutputFactory.create_batch(3)
-        LowerLevelOutputFactory.create_batch(3)
+        generate_fake_data(quantity=3)
+
+        print "Fake data are generated! :D"
