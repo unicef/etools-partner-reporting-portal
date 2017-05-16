5e272dc6fb23725485805abf80b9be83f93ba7a5
Silin Na <n1207n@gmail.com>
2017-05-16 16:25:51 -0700

    refactor: Changes to Indicator List API endpoint [#19]
    + Added ref_num and achieved API field values
    
    + Limited queryset for IndicatorListCreateAPIView into Reportables to LowerLevelOutput with IndicatorReport

--- django_api/django_api/apps/indicator/serializers.py
+++ django_api/django_api/apps/indicator/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from unicef.models import LowerLevelOutput
+
 from .models import Reportable, IndicatorBlueprint
 
 
@@ -8,15 +10,30 @@ class IndicatorBlueprintSimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = IndicatorBlueprint
         fields = (
-            'id', 'title', 'unit', 'code'
+            'id', 'title', 'unit',
         )
 
 
 class IndicatorListSerializer(serializers.ModelSerializer):
     blueprint = IndicatorBlueprintSimpleSerializer()
+    ref_num = serializers.SerializerMethodField()
+    achieved = serializers.SerializerMethodField()
+
+    def get_ref_num(self, obj):
+        if isinstance(obj.content_object, LowerLevelOutput):
+            return obj.content_object.indicator.programme_document.ref
+        else:
+            return ''
+
+    def get_achieved(self, obj):
+        if obj.indicator_reports.exists():
+            return obj.indicator_reports.last().total
+        else:
+            return None
 
     class Meta:
         model = Reportable
         fields = (
-            'id', 'target', 'baseline', 'blueprint'
+            'id', 'target', 'baseline', 'blueprint',
+            'ref_num', 'achieved'
         )
--- django_api/django_api/apps/indicator/views.py
+++ django_api/django_api/apps/indicator/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
 
 from rest_framework.generics import ListCreateAPIView
+
+from unicef.models import LowerLevelOutput
 
 from .serializers import IndicatorListSerializer
 from .models import Reportable
@@ -10,5 +13,7 @@ class IndicatorListCreateAPIView(ListCreateAPIView):
     """
     REST API endpoint to get a list of Indicator objects and to create a new Indicator object.
     """
-    queryset = Reportable.objects.all()
     serializer_class = IndicatorListSerializer
+
+    def get_queryset(self):
+        return Reportable.objects.filter(indicator_reports__isnull=False, content_type=ContentType.objects.get_for_model(LowerLevelOutput))
