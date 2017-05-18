795d5764d166d0f10d786877374010c626da43f3
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-05-18 13:29:21 +0200

    first filter on PD list api view list

---
+++ django_api/django_api/apps/unicef/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+class ProgrammeDocumentForm(forms.Form):
+
+    ref_title = forms.CharField(required=False, max_length=256)
+    status = forms.CharField(required=False, max_length=256)
+    locations = forms.CharField(required=False, max_length=256)
+    report_status = forms.CharField(required=False, max_length=128)
+    due_date = forms.DateField(
+        required=False,
+        help_text=(
+            "Valid format data is '%m/%d/%Y', '%m-%d-%Y', '%m%d%Y', '%Y-%m-%d'."
+        ),
+        input_formats=[
+            '%m/%d/%Y',
+            '%m-%d-%Y',
+            '%m%d%Y',
+            '%Y-%m-%d'
+        ]
+    )
--- django_api/django_api/apps/unicef/views.py
+++ django_api/django_api/apps/unicef/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
+
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status as statuses
 
 from core.permissions import IsAuthenticated
 from .serializer import (
@@ -8,19 +10,41 @@ from .serializer import (
 )
 
 from .models import ProgrammeDocument
+from .forms import ProgrammeDocumentForm
 
 
 class ProgrammeDocumentAPIView(ListAPIView):
     """
     Endpoint for getting Partner Details for overview tab.
     """
-    queryset = ProgrammeDocument.objects.all()
     serializer_class = ProgrammeDocumentSerializer
     permission_classes = (IsAuthenticated, )
 
-    # def get(self, request, *args, **kwargs):
-    #     """
-    #     Get Programme Document list.
-    #     """
-    #     serializer = self.get_serializer()
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
+    def get_queryset(self, form=None):
+        # remember to do pagination !!!
+        if form is None:
+            return ProgrammeDocument.objects.all()
+
+        queryset = ProgrammeDocument.objects
+
+        ref_title = form.cleaned_data.get('ref_title')
+        if ref_title:
+            queryset = queryset.filter(
+                Q(reference_number__icontains=ref_title) | Q(title__icontains=ref_title)
+            )
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        """
+        Get Programme Document list.
+        """
+        if request.GET:
+            form = ProgrammeDocumentForm(request.GET)
+            if not form.is_valid():
+                return Response(form.errors.items())
+            queryset = self.get_queryset(form)
+        else:
+            queryset = self.get_queryset(request)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=statuses.HTTP_200_OK)
