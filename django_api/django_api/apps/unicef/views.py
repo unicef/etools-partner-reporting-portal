d6611ab536df62bca2b0b59c783036f8024afbd0
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-05-22 14:09:04 +0200

    change the way of filtering queryset - start using django-filter version 1.0.4

--- django_api/django_api/apps/unicef/views.py
+++ django_api/django_api/apps/unicef/views.py
@@ -1,5 +1,3 @@
-from django.db.models import Q
-
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from rest_framework.response import Response
 from rest_framework import status as statuses
@@ -10,46 +8,29 @@ from .serializer import (
 )
 
 from .models import ProgrammeDocument
-from .forms import ProgrammeDocumentForm
+from .filters import ProgrammeDocumentFilter
 
 
 class ProgrammeDocumentAPIView(ListAPIView):
     """
-    Endpoint for getting Partner Details for overview tab.
+    Endpoint for getting Programme Document.
     """
     serializer_class = ProgrammeDocumentSerializer
     permission_classes = (IsAuthenticated, )
 
-    def get_queryset(self, form=None):
-        # remember to do pagination !!!
-        if form is None:
-            return ProgrammeDocument.objects.all()
-
-        queryset = ProgrammeDocument.objects
-        ref_title = form.cleaned_data.get('ref_title')
-        if ref_title:
-            queryset = queryset.filter(
-                Q(reference_number__icontains=ref_title) | Q(title__icontains=ref_title)
-            )
-
-        status = form.cleaned_data.get('status')
-        if status:
-            queryset = queryset.filter(status__icontains=status)
-
-
-
-        return queryset
+    def get_queryset(self):
+        return ProgrammeDocument.objects.all()
+        # TODO: we should filter PD to partner I guess?
+        # return ProgrammeDocument.objects.filter(reportable__project__partner=self.request.user.partner)
 
     def list(self, request, *args, **kwargs):
         """
         Get Programme Document list.
         """
-        if request.GET:
-            form = ProgrammeDocumentForm(request.GET)
-            if not form.is_valid():
-                return Response(form.errors.items())
-            queryset = self.get_queryset(form)
-        else:
-            queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data, status=statuses.HTTP_200_OK)
+        queryset = self.get_queryset()
+        filtered = ProgrammeDocumentFilter(request.GET, queryset=queryset)
+        serializer = self.get_serializer(filtered.qs, many=True)
+        return Response(
+            serializer.data,
+            status=statuses.HTTP_200_OK
+        )
