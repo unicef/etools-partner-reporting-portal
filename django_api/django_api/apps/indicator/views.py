daff39f827405e581d961327309af41f51f53fbd
Silin Na <n1207n@gmail.com>
2017-05-22 15:06:22 -0700

    feat: Added filtering options that deal with GenericRelation in APIView [#20]

--- django_api/django_api/apps/indicator/views.py
+++ django_api/django_api/apps/indicator/views.py
@@ -1,3 +1,6 @@
+import operator
+
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 
@@ -23,5 +26,32 @@ class IndicatorListCreateAPIView(ListCreateAPIView):
 
     def get_queryset(self):
         queryset = Reportable.objects.filter(indicator_reports__isnull=False, content_type=ContentType.objects.get_for_model(LowerLevelOutput))
+
+        q_list = []
+
+        locations = self.request.query_params.get('locations', None)
+        pds = self.request.query_params.get('pds', None)
+        clusters = self.request.query_params.get('clusters', None)
+        # TODO: Turn this back on when feature-pd-list branch is merged
+        # pd_statuses = self.request.query_params.get('pd_statuses', None)
+
+        if locations:
+            location_list = map(lambda item: int(item), locations.split(','))
+            q_list.append(Q(locations__id__in=location_list))
+
+        if pds:
+            pd_list = map(lambda item: int(item), pds.split(','))
+            q_list.append(Q(lower_level_outputs__indicator__programme_document__id__in=pd_list))
+
+        if clusters:
+            cluster_list = map(lambda item: int(item), clusters.split(','))
+            q_list.append(Q(cluster_activities__cluster__id__in=cluster_list))
+
+        # if pd_statuses:
+        #     pd_status_list = map(lambda item: int(item), pd_statuses.split(','))
+        #     q_list.append(Q(lower_level_outputs__indicator__programme_document__status__in=pd_status_list))
+
+        if q_list:
+            queryset = queryset.filter(reduce(operator.or_, q_list))
 
         return queryset
