8ac03963e5e5604a10dc319d133b5023d5c48e13
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-05-29 13:57:46 +0200

    minor improvement

--- django_api/django_api/apps/indicator/views.py
+++ django_api/django_api/apps/indicator/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status as statuses
 from rest_framework.generics import ListAPIView
@@ -17,9 +18,11 @@ class PDReportsAPIView(ListAPIView):
     # filter_class = PDReportsFilter
 
     def get_queryset(self):
-        # draft
         from unicef.models import ProgrammeDocument
-        pd = ProgrammeDocument.objects.get(pk=self.pd_id)
+        try:
+            pd = ProgrammeDocument.objects.get(pk=self.pd_id)
+        except ProgrammeDocument.DoesNotExist:
+            raise Http404
         pks = pd.reportable_queryset.values_list('indicator_reports__pk', flat=True)
         return IndicatorReport.objects.filter(id__in=pks)
 
