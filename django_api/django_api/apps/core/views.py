0ddcec135ddc4096ace33a6ad6fa7859d12be655
Silin Na <n1207n@gmail.com>
2017-05-30 15:15:34 -0700

    test: Fixed Django unit test for Intervention [#20]
    + Added M2M locations to InterventionFactory

--- django_api/django_api/apps/core/views.py
+++ django_api/django_api/apps/core/views.py
@@ -13,7 +13,7 @@ class SimpleInterventionAPIView(ListAPIView):
     Endpoint for getting Intervention.
     Intervention need to have defined location to be displayed on drop down menu.
     """
-    queryset = Intervention.objects.filter(locations__isnull=False)
+    queryset = Intervention.objects.prefetch_related('locations').filter(locations__isnull=False).distinct()
     serializer_class = SimpleInterventionSerializer
     permission_classes = (IsAuthenticated, )
 
