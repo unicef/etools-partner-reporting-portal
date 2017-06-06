d743b1554fa3f59d5ca1bd60a56807fb81f4c3b2
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-06-06 15:22:02 +0200

    create BaseAPITestCase

--- django_api/django_api/apps/indicator/tests/test_views.py
+++ django_api/django_api/apps/indicator/tests/test_views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse
-
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from account.models import User
+from core.tests.base import BaseAPITestCase
 from core.factories import (
     IndicatorReportFactory, ProgrammeDocumentFactory, ReportableToLowerLevelOutputFactory,
     ProgressReportFactory,
@@ -11,27 +11,16 @@ from core.factories import (
     IndicatorLocationDataFactory,
 )
 from indicator.models import IndicatorReport
-
 from unicef.models import LowerLevelOutput, Section, ProgrammeDocument
 
 
-class TestPDReportsAPIView(APITestCase):
+class TestPDReportsAPIView(BaseAPITestCase):
+
+    generate_fake_data_quantity = 5
 
     def setUp(self):
-        self.quantity = 5
-
-        ProgrammeDocumentFactory.create_batch(self.quantity)
-        print "{} ProgrammeDocument objects created".format(self.quantity)
-
-        SectionFactory.create_batch(self.quantity)
-        print "{} Section objects created".format(self.quantity)
-
-        # Linking the followings:
-        # created LowerLevelOutput - ReportableToLowerLevelOutput
-        # Section - ProgrammeDocument via ReportableToLowerLevelOutput
-        # ProgressReport - IndicatorReport from ReportableToLowerLevelOutput
-        # IndicatorReport & Location from ReportableToLowerLevelOutput - IndicatorLocationData
-        for idx in xrange(self.quantity):
+        super(TestPDReportsAPIView, self).setUp()
+        for idx in xrange(self.generate_fake_data_quantity):
             llo = LowerLevelOutput.objects.all()[idx]
             reportable = ReportableToLowerLevelOutputFactory(content_object=llo)
 
@@ -41,18 +30,10 @@ class TestPDReportsAPIView(APITestCase):
             indicator_report.progress_report = ProgressReportFactory()
             indicator_report.save()
 
-            indicator_location_data = IndicatorLocationDataFactory(indicator_report=indicator_report, location=reportable.locations.first())
-
-        # Make all requests in the context of a logged in session.
-        admin, created = User.objects.get_or_create(username='admin', defaults={
-            'email': 'admin@unicef.org',
-            'is_superuser': True,
-            'is_staff': True
-        })
-        admin.set_password('Passw0rd!')
-        admin.save()
-        self.client = APIClient()
-        self.client.login(username='admin', password='Passw0rd!')
+            indicator_location_data = IndicatorLocationDataFactory(
+                indicator_report=indicator_report,
+                location=reportable.locations.first()
+            )
 
     def test_list_api(self):
         pd = ProgrammeDocument.objects.first()
