dbcf31eb0465c94b90afd88003a7590eb4294fd2
Silin Na <n1207n@gmail.com>
2017-06-08 11:30:24 -0700

    fix: Resolved Django db migration merge conflicts [URGENT]

--- django_api/django_api/apps/unicef/migrations/0004_auto_20170528_2008.py
+++ django_api/django_api/apps/unicef/migrations/0004_auto_20170528_2008.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('unicef', '0002_progressreport_status'),
+        ('unicef', '0003_auto_20170528_2133'),
     ]
 
     operations = [
