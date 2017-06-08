ca2c3c9579bbf591ee3232267234979b125d328d
Silin Na <n1207n@gmail.com>
2017-06-08 14:25:10 -0700

    feat: Created IndicatorReportDetailAPIView class [#34]
    + serializer
    
    + URL pattern

--- django_api/django_api/apps/indicator/urls.py
+++ django_api/django_api/apps/indicator/urls.py
@@ -4,6 +4,7 @@ from .views import (
     PDReportsAPIView,
     IndicatorListCreateAPIView,
     IndicatorReportListAPIView,
+    IndicatorReportDetailAPIView,
 )
 
 
@@ -12,5 +13,6 @@ urlpatterns = [
         PDReportsAPIView.as_view(),
         name="programme-document-reports"),
     url(r'^$', IndicatorListCreateAPIView.as_view(), name='indicator-list-create-api'),
+    url(r'^(?P<pk>\d+)/indicator-reports/(?P<indicator_report_pk>\d+)$', IndicatorReportDetailAPIView.as_view(), name='indicator-report-detail-api'),
     url(r'^(?P<pk>\d+)/indicator-reports/$', IndicatorReportListAPIView.as_view(), name='indicator-report-list-api'),
 ]
