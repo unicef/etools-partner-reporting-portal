e362dfe9e4c1ab8f9c7f5c43e0d1d239ab211c43
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-06-08 17:04:26 +0200

    update model ClusterObjective

--- django_api/django_api/apps/cluster/models.py
+++ django_api/django_api/apps/cluster/models.py
@@ -5,6 +5,8 @@ from django.contrib.contenttypes.fields import GenericRelation
 
 from model_utils.models import TimeStampedModel
 
+from core.common import FREQUENCY_LEVEL
+
 
 class Cluster(TimeStampedModel):
     title = models.CharField(max_length=255)
@@ -12,16 +14,29 @@ class Cluster(TimeStampedModel):
     user = models.ForeignKey('account.User', related_name="clusters")
 
     def __str__(self):
-        return self.title
+        return "<pk: %s> %s" % (self.id, self.title)
 
 
 class ClusterObjective(TimeStampedModel):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Cluster Objective Title')
+    reference_number = models.CharField(max_length=255, verbose_name='Reference Number')
     cluster = models.ForeignKey(Cluster, related_name="cluster_objectives")
+    frequency = models.CharField(
+        max_length=3,
+        choices=FREQUENCY_LEVEL,
+        default=FREQUENCY_LEVEL.monthly,
+        verbose_name='Frequency of reporting'
+    )
     reportables = GenericRelation('indicator.Reportable', related_query_name='cluster_objectives')
+
+    def __str__(self):
+        return "<pk: %s> %s" % (self.id, self.title)
 
 
 class ClusterActivity(TimeStampedModel):
     title = models.CharField(max_length=255)
     cluster_objective = models.ForeignKey(ClusterObjective, related_name="cluster_activities")
     reportables = GenericRelation('indicator.Reportable', related_query_name='cluster_activities')
+
+    def __str__(self):
+        return "<pk: %s> %s" % (self.id, self.title)
