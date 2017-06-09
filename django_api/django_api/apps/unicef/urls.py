9cb834dabdf8d3759a13fd46d3313a9addbee0f0
Adam Lord <adam.lord@gmail.com>
2017-06-08 17:16:14 -0700

    feat: get ProgressReports by location_id

--- django_api/django_api/apps/unicef/urls.py
+++ django_api/django_api/apps/unicef/urls.py
@@ -10,5 +10,5 @@ from .views import (
 urlpatterns = [
     url(r'^(?P<location_id>\d+)/programme-document/$', ProgrammeDocumentAPIView.as_view(), name="programme-document"),
     url(r'^(?P<location_id>\d+)/programme-document/details/(?P<pk>\d+)/$', ProgrammeDocumentDetailsAPIView.as_view(), name="programme-document-details"),
-    url(r'^progress-reports/$', ProgressReportAPIView.as_view(), name="progress-reports"),
+    url(r'^(?P<location_id>\d+)/progress-reports/$', ProgressReportAPIView.as_view(), name="progress-reports"),
 ]
--- django_api/django_api/apps/unicef/views.py
+++ django_api/django_api/apps/unicef/views.py
@@ -87,10 +87,38 @@ class ProgressReportAPIView(ListAPIView):
     """
     Endpoint for getting list of all PD Progress Reports
     """
-    queryset = ProgressReport.objects.all()
+    # queryset = ProgressReport.objects.all()
     serializer_class = ProgressReportSerializer
     pagination_class = SmallPagination
     permission_classes = (IsAuthenticated, )
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend, )
     filter_class = ProgressReportFilter
 
+    def get_queryset(self):
+        pd_ids = Location.objects.filter(
+            Q(id=self.location_id) |
+            Q(parent_id=self.location_id) |
+            Q(parent__parent_id=self.location_id) |
+            Q(parent__parent__parent_id=self.location_id) |
+            Q(parent__parent__parent__parent_id=self.location_id)
+        ).values_list(
+             'reportable__lower_level_outputs__indicator__programme_document__id',
+             flat=True
+        )
+        return ProgressReport.objects.filter(programme_document_id__in=pd_ids)
+
+    def list(self, request, location_id, *args, **kwargs):
+        self.location_id = location_id
+        queryset = self.get_queryset()
+        filtered = ProgressReportFilter(request.GET, queryset=queryset)
+
+        page = self.paginate_queryset(filtered.qs)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(filtered.qs, many=True)
+        return Response(
+            serializer.data,
+            status=statuses.HTTP_200_OK
+        )
