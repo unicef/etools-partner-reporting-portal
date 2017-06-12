dc855e4ffdda66a3bb9feb211434112f00acb089
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-06-12 10:33:29 +0200

    split test_crud_cluster_objective to smaller function like create, update, delete, read

--- django_api/django_api/apps/cluster/tests/test_views.py
+++ django_api/django_api/apps/cluster/tests/test_views.py
@@ -2,12 +2,24 @@ from django.urls import reverse
 from rest_framework import status
 from core.tests.base import BaseAPITestCase
 from core.common import FREQUENCY_LEVEL
-from cluster.models import ClusterObjective
+from cluster.models import ClusterObjective, Cluster
 
 
 class TestClusterObjectiveAPIView(BaseAPITestCase):
 
-    def test_crud_cluster_objective(self):
+    title = "client post title"
+    reference_number = "ref no 123"
+
+    @property
+    def data(self):
+        return {
+            "cluster": Cluster.objects.first().id,
+            "reference_number": self.reference_number,
+            "title": self.title,
+            "frequency": FREQUENCY_LEVEL.weekly,
+        }
+
+    def test_create_cluster_objective(self):
         """
         create and update unit test for ClusterObjectiveAPIView
         :return:
@@ -17,41 +29,48 @@ class TestClusterObjectiveAPIView(BaseAPITestCase):
 
         # test for creating object
         url = reverse('cluster-objective')
-        title = "client post title"
-        reference_number = "ref no 123"
-        data = {
-            "cluster": 1,
-            "reference_number": reference_number,
-            "title": title,
-            "frequency": FREQUENCY_LEVEL.weekly,
-        }
-        response = self.client.post(url, data=data, format='json')
+        response = self.client.post(url, data=self.data, format='json')
 
         self.assertTrue(status.is_success(response.status_code))
         self.assertEquals(response.data['id'], (last.id+1))
         created_obj = ClusterObjective.objects.get(id=response.data['id'])
         self.assertEquals(created_obj.reference_number, "ref no 123")
         self.assertEquals(created_obj.frequency, FREQUENCY_LEVEL.weekly)
-        self.assertTrue(ClusterObjective.objects.all().count() > base_count)
+        self.assertEquals(ClusterObjective.objects.all().count(),  base_count + 1)
 
-        # test for updating object
-        new_count = base_count + 1
-        obj_to_update_id = response.data['id']
-        data.update(dict(id=obj_to_update_id))
+    def test_update_cluster_objective(self):
+        """
+        update object unit test for ClusterObjectiveAPIView
+        """
+        base_count = ClusterObjective.objects.all().count()
+        last = ClusterObjective.objects.last()
+
+        data = self.data
+        data.update(dict(id=last.id))
         data['title'] = 'new updated title'
+        url = reverse('cluster-objective')
         response = self.client.post(url, data=data, format='json')
         self.assertTrue(status.is_success(response.status_code))
-        self.assertEquals(ClusterObjective.objects.all().count(), new_count)
+        self.assertEquals(ClusterObjective.objects.all().count(), base_count)
         self.assertEquals(ClusterObjective.objects.get(id=response.data['id']).title, data['title'])
-        self.assertFalse((ClusterObjective.objects.filter(title=title).exists()))
 
-        # test for deleting object
-        response = self.client.delete(url, data=data, format='json')
+    def test_delete_cluster_objective(self):
+        """
+        delete object unit test for ClusterObjectiveAPIView
+        """
+        base_count = ClusterObjective.objects.all().count()
+        last = ClusterObjective.objects.last()
+        url = reverse('cluster-objective')
+        response = self.client.delete(url, data={"id": last.pk}, format='json')
         self.assertTrue(status.is_success(response.status_code))
         self.assertEquals(response.data, None)
-        self.assertEquals(ClusterObjective.objects.all().count(), base_count)
+        self.assertEquals(ClusterObjective.objects.all().count(), base_count-1)
 
-        # test for getting object by given pk
+    def test_read_cluster_objective(self):
+        """
+        read object unit test for ClusterObjectiveAPIView
+        """
+        last = ClusterObjective.objects.last()
         url = reverse('cluster-objective', kwargs={"pk": last.pk})
         response = self.client.get(url, format='json')
         self.assertTrue(status.is_success(response.status_code))
@@ -65,9 +84,12 @@ class TestClusterObjectiveAPIView(BaseAPITestCase):
         self.assertEquals(response.data['count'], ClusterObjective.objects.all().count())
 
         # test for getting objects by given filter parameter title or reference number
-        response = self.client.get(url + "?ref_title=2", format='json')
+        response = self.client.get(url + "?ref_title=%s"%last.title[10:], format='json')
         self.assertTrue(status.is_success(response.status_code))
         self.assertEquals(response.data['count'], 1)
+        response = self.client.get(url + "?ref_title=%s"%last.title[:10], format='json')
+        self.assertTrue(status.is_success(response.status_code))
+        self.assertEquals(response.data['count'], 3)
 
         # test for defined cluster
         url = reverse('cluster-objective-list', kwargs={'cluster_id': last.cluster_id})
