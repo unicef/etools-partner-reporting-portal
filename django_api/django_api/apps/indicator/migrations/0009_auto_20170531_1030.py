890773ac4b0b07b9208a573673063660aa618975
Silin Na <n1207n@gmail.com>
2017-06-19 13:05:09 -0700

    refactor: Moved feature-branch db migration to next numbering [#122]

--- django_api/django_api/apps/indicator/migrations/0009_auto_20170531_1030.py
+++ django_api/django_api/apps/indicator/migrations/0009_auto_20170531_1030.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('indicator', '0007_remove_indicatorreport_location'),
+        ('indicator', '0008_auto_20170613_2011'),
     ]
 
     operations = [
