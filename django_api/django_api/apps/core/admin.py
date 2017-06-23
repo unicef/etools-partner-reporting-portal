b4b350604592d5f4c4173c1aed007a251d6b52a4
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-06-23 14:31:44 +0200

    add Response Plan model to admin panel

--- django_api/django_api/apps/core/admin.py
+++ django_api/django_api/apps/core/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import (
     Intervention,
     Location,
+    ResponsePlan,
 )
 
 admin.site.register(Intervention)
 admin.site.register(Location)
+admin.site.register(ResponsePlan)
