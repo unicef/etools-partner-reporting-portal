7350315c7fd89aea402b95e3335f74d0cfdc02d5
Silin Na <n1207n@gmail.com>
2017-07-12 12:59:38 -0700

    test: Created helper test case classes [#163]

---
+++ django_api/django_api/apps/core/tests/test_helpers.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import random
+from ast import literal_eval
+from contextlib import contextmanager
+from itertools import combinations, product
+from collections import OrderedDict
+
+from django.test import TestCase
+
+
+class TestCombinatorics(TestCase):
+    def setUp(self):
+        pass
+
+    def get_combination_pairs(self):
+        pass
+
+    def generate_data_combination_entries(self):
+        pass
+
+
+class TestDictionaryHelpers(TestCase):
+    def get_sorted_ordered_dict_by_keys(self):
+        pass
+
+    def get_cast_dictionary_keys_as_tuple(self):
+        pass
+
+    def get_cast_dictionary_keys_as_string(self):
+        pass
