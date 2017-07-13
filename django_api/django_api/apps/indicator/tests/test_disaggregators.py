9fb3188c99a30e16b9dcd677b91bbb74145caa48
Silin Na <n1207n@gmail.com>
2017-07-13 16:16:17 -0700

    test: Added test cases for reporting period [#163]
    + QuantityIndicatorDisaggregator
    
    + RatioIndicatorDisaggregator

--- django_api/django_api/apps/indicator/tests/test_disaggregators.py
+++ django_api/django_api/apps/indicator/tests/test_disaggregators.py
@@ -75,6 +75,69 @@ class TestQuantityIndicatorDisaggregator(BaseAPITestCase):
 
         self.assertEquals(indicator_report.total['c'], avg_value)
 
+    def test_post_process_reporting_period_sum_calc(self):
+        unit_type = IndicatorBlueprint.NUMBER
+        calc_type = IndicatorBlueprint.SUM
+
+        indicator = Reportable.objects.filter(
+            blueprint__unit=unit_type,
+            blueprint__calculation_formula_across_periods=calc_type,
+        ).first()
+
+        indicator_report = indicator.indicator_reports.first()
+        report_total = 0
+
+        for loc_data in indicator_report.indicator_location_data.all():
+            QuantityIndicatorDisaggregator.post_process(loc_data)
+
+        for report in indicator.indicator_reports.all():
+            report_total += report.total['c']
+
+        self.assertEquals(indicator.total['c'], report_total)
+
+    def test_post_process_reporting_period_max_calc(self):
+        unit_type = IndicatorBlueprint.NUMBER
+        calc_type = IndicatorBlueprint.MAX
+
+        indicator = Reportable.objects.filter(
+            blueprint__unit=unit_type,
+            blueprint__calculation_formula_across_periods=calc_type,
+        ).first()
+
+        indicator_report = indicator.indicator_reports.first()
+        report_total = 0
+
+        for loc_data in indicator_report.indicator_location_data.all():
+            QuantityIndicatorDisaggregator.post_process(loc_data)
+
+        for report in indicator.indicator_reports.all():
+            if report.total['c'] > report_total:
+                report_total = report.total['c']
+
+        self.assertEquals(indicator.total['c'], report_total)
+
+    def test_post_process_reporting_period_avg_calc(self):
+        unit_type = IndicatorBlueprint.NUMBER
+        calc_type = IndicatorBlueprint.AVG
+
+        indicator = Reportable.objects.filter(
+            blueprint__unit=unit_type,
+            blueprint__calculation_formula_across_periods=calc_type,
+        ).first()
+
+        indicator_report = indicator.indicator_reports.first()
+        report_total = 0
+
+        for loc_data in indicator_report.indicator_location_data.all():
+            QuantityIndicatorDisaggregator.post_process(loc_data)
+
+        for report in indicator.indicator_reports.all():
+            report_total += report.total['c']
+
+        report_total /= (indicator.indicator_reports.count() * 1.0)
+
+        self.assertEquals(indicator.total['c'], report_total)
+
 
 class TestRatioIndicatorDisaggregator(BaseAPITestCase):
     generate_fake_data_quantity = 40
@@ -101,3 +164,28 @@ class TestRatioIndicatorDisaggregator(BaseAPITestCase):
         ratio_value = v_total / (d_total * 1.0)
 
         self.assertEquals(indicator_report.total['c'], ratio_value)
+
+    def test_post_process_reporting_period_ratio_calc(self):
+        unit_type = IndicatorBlueprint.PERCENTAGE
+        calc_type = IndicatorBlueprint.RATIO
+
+        indicator = Reportable.objects.filter(
+            blueprint__unit=unit_type,
+            blueprint__calculation_formula_across_periods=calc_type,
+        ).first()
+
+        indicator_report = indicator.indicator_reports.first()
+        v_total = 0
+        d_total = 0
+        ratio_value = 0
+
+        for loc_data in indicator_report.indicator_location_data.all():
+            RatioIndicatorDisaggregator.post_process(loc_data)
+
+        for report in indicator.indicator_reports.all():
+            v_total += report.total['v']
+            d_total += report.total['d']
+
+        ratio_value = v_total / (d_total * 1.0)
+
+        self.assertEquals(indicator.total['c'], ratio_value)
