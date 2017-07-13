31cee3bddb7b8ca045a780bd5bcd7d7b901e4fd1
Silin Na <n1207n@gmail.com>
2017-07-12 17:30:02 -0700

    test: Added test cases for TestQuantityIndicatorDisaggregator and TestRatioIndicatorDisaggregator [#163]

--- django_api/django_api/apps/indicator/tests/test_disaggregators.py
+++ django_api/django_api/apps/indicator/tests/test_disaggregators.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 
-from django.test import TestCase
-
+from core.tests.base import BaseAPITestCase
 from core.helpers import (
     generate_data_combination_entries,
     get_cast_dictionary_keys_as_tuple,
@@ -15,19 +14,61 @@ from indicator.disaggregators import (
 )
 
 
-class TestQuantityIndicatorDisaggregator(TestCase):
+class TestQuantityIndicatorDisaggregator(BaseAPITestCase):
+    generate_fake_data_quantity = 40
 
-    def setUp(self):
-        pass
+    def test_post_process_location_sum_calc(self):
+        number_type = IndicatorBlueprint.NUMBER
+        sum_type = IndicatorBlueprint.SUM
 
-    def test_post_process(self):
-        pass
+        quantity_type_indicator = Reportable.objects.filter(
+            blueprint__unit=number_type,
+            blueprint__calculation_formula_across_locations=sum_type,
+        ).first()
+
+        indicator_location_data = IndicatorLocationData.objects.filter(
+            indicator_report__reportable=quantity_type_indicator,
+        ).first()
+
+    def test_post_process_location_max_calc(self):
+        number_type = IndicatorBlueprint.NUMBER
+        max_type = IndicatorBlueprint.MAX
+
+        quantity_type_indicator = Reportable.objects.filter(
+            blueprint__unit=number_type,
+            blueprint__calculation_formula_across_locations=max_type,
+        ).first()
+
+        indicator_location_data = IndicatorLocationData.objects.filter(
+            indicator_report__reportable=quantity_type_indicator,
+        ).first()
+
+    def test_post_process_location_avg_calc(self):
+        number_type = IndicatorBlueprint.NUMBER
+        avg_type = IndicatorBlueprint.AVG
+
+        quantity_type_indicator = Reportable.objects.filter(
+            blueprint__unit=number_type,
+            blueprint__calculation_formula_across_locations=avg_type,
+        ).first()
+
+        indicator_location_data = IndicatorLocationData.objects.filter(
+            indicator_report__reportable=quantity_type_indicator,
+        ).first()
 
 
-class TestRatioIndicatorDisaggregator(TestCase):
+class TestRatioIndicatorDisaggregator(BaseAPITestCase):
+    generate_fake_data_quantity = 40
 
-    def setUp(self):
-        pass
+    def test_post_process_location_percentage_calc(self):
+        number_type = IndicatorBlueprint.RATIO
+        percentage_type = IndicatorBlueprint.PERCENTAGE
 
-    def test_post_process(self):
-        pass
+        quantity_type_indicator = Reportable.objects.filter(
+            blueprint__unit=number_type,
+            blueprint__calculation_formula_across_locations=percentage_type,
+        ).first()
+
+        indicator_location_data = IndicatorLocationData.objects.filter(
+            indicator_report__reportable=quantity_type_indicator,
+        ).first()
