1e203870d14794ae3d46d66ac34b6d4f2ee39feb
Silin Na <n1207n@gmail.com>
2017-07-21 17:23:24 -0700

    feat: Added a logic for Cluster reporting Indicator processing [#45]
    + Added few print statements

--- django_api/django_api/apps/core/management/commands/generate_reports_for_periods.py
+++ django_api/django_api/apps/core/management/commands/generate_reports_for_periods.py
@@ -74,12 +74,15 @@ class Command(BaseCommand):
                 date_list = find_missing_frequency_period_dates(
                     pd.start_date, None, frequency)
 
+            print "Missing dates: {}".format(date_list)
+
             with transaction.atomic():
                 for missing_date in date_list:
                     end_date = calculate_end_date_given_start_date(
                         missing_date, frequency)
 
                     # Create ProgressReport first
+                    print "Creating ProgressReport object for {} - {}".format(pd, missing_date)
                     next_progress_report = ProgressReportFactory(
                         start_date=missing_date,
                         end_date=end_date,
@@ -88,6 +91,7 @@ class Command(BaseCommand):
 
                     for reportable in reportable_queryset:
                         if reportable.blueprint.unit == IndicatorBlueprint.NUMBER:
+                            print "Creating Quantity IndicatorReport object for {} - {}".format(pd, missing_date)
                             indicator_report = QuantityIndicatorReportFactory(
                                 reportable=reportable,
                                 time_period_start=missing_date,
@@ -95,6 +99,7 @@ class Command(BaseCommand):
                             )
 
                             for location in reportable.locations.all():
+                                print "Creating IndicatorLocationData object for {}".format(indicator_report)
                                 location_data = IndicatorLocationDataFactory(
                                     indicator_report=indicator_report,
                                     location=location,
@@ -106,6 +111,7 @@ class Command(BaseCommand):
                                 )
 
                         else:
+                            print "Creating Ratio IndicatorReport object for {} - {}".format(pd, missing_date)
                             indicator_report = RatioIndicatorReportFactory(
                                 reportable=reportable,
                                 time_period_start=missing_date,
@@ -113,6 +119,7 @@ class Command(BaseCommand):
                             )
 
                             for location in reportable.locations.all():
+                                print "Creating IndicatorLocationData object {} - {}".format(indicator_report, missing_date)
                                 location_data = IndicatorLocationDataFactory(
                                     indicator_report=indicator_report,
                                     location=location,
@@ -126,66 +133,73 @@ class Command(BaseCommand):
                         indicator_report.progress_report = progress_report
                         indicator_report.save()
 
-        # # TODO: Add active flag to Reportable
-        # for indicator in Reportable.objects.filter(
-        #     content_type__model__in=[
-        #         'partnerproject', 'partneractivity', 'clusterobjective']):
-        #     frequency = indicator.frequency
-        #     latest_indicator_report = indicator.indicator_reports.last()
-        #
-        #     if not latest_indicator_report:
-        #         if indicator.frequency == PD_FREQUENCY_LEVEL.weekly:
-        #             end_date = indicator.start_date + timedelta(7)
-        #
-        #         elif indicator.frequency == PD_FREQUENCY_LEVEL.monthly:
-        #             num_of_days = get_num_of_days_in_a_month(
-        #                 indicator.start_date.year, indicator.start_date.month)
-        #
-        #             end_date = date(
-        #                 indicator.start_date.year, indicator.start_date.month, num_of_days)
-        #
-        #         elif indicator.frequency == PD_FREQUENCY_LEVEL.quarterly:
-        #             quarter = get_current_quarter_for_a_month(
-        #                 indicator.start_date.month)
-        #
-        #             end_date = get_first_date_of_a_quarter(
-        #                 indicator.start_date.year, quarter=quarter)
-        #
-        #         # TODO: Handling custom_specific_dates later
-        #         # elif indicator.frequency == PD_FREQUENCY_LEVEL.custom_specific_dates:
-        #         #     end_date = indicator.start_date + timedelta(7)
-        #
-        #         if reportable.blueprint.unit == IndicatorBlueprint.NUMBER:
-        #             latest_indicator_report = QuantityIndicatorReportFactory(
-        #                 reportable=indicator,
-        #                 time_period_start=indicator.start_date,
-        #                 time_period_end=end_date,
-        #             )
-        #         else:
-        #             latest_indicator_report = RatioIndicatorReportFactory(
-        #                 reportable=indicator,
-        #                 time_period_start=indicator.start_date,
-        #                 time_period_end=end_date,
-        #             )
-        #
-        #     if latest_indicator_report:
-        #         start_date = latest_indicator_report.start_date
-        #         last_date = latest_indicator_report.end_date
-        #
-        #     date_list = find_missing_frequency_period_dates(
-        #         start_date, last_date, frequency)
-        #
-        #     with transaction.atomic():
-        #         for missing_date in date_list:
-        #             if reportable.blueprint.unit == IndicatorBlueprint.NUMBER:
-        #                 indicator_report = QuantityIndicatorReportFactory(
-        #                     reportable=indicator,
-        #                     time_period_start=indicator.start_date,
-        #                     time_period_end=end_date,
-        #                 )
-        #             else:
-        #                 indicator_report = RatioIndicatorReportFactory(
-        #                     reportable=indicator,
-        #                     time_period_start=indicator.start_date,
-        #                     time_period_end=end_date,
-        #                 )
+        # TODO: Add active flag to Reportable
+        for indicator in Reportable.objects.filter(
+            content_type__model__in=[
+                'partnerproject', 'partneractivity', 'clusterobjective']):
+            print "Processing Reportable {}".format(indicator)
+
+            frequency = indicator.frequency
+            latest_indicator_report = indicator.indicator_reports.last()
+
+            # Get missing date list based on progress report existence
+            if latest_indicator_report:
+                date_list = find_missing_frequency_period_dates(
+                    indicator.start_date,
+                    latest_indicator_report.end_date,
+                    frequency,
+                )
+
+            else:
+                date_list = find_missing_frequency_period_dates(
+                    indicator.start_date, None, frequency)
+
+            print "Missing dates: {}".format(date_list)
+
+            with transaction.atomic():
+                for missing_date in date_list:
+                    end_date = calculate_end_date_given_start_date(
+                        missing_date, frequency)
+
+                    if indicator.blueprint.unit == IndicatorBlueprint.NUMBER:
+                        print "Creating Quantity IndicatorReport object for {} - {}".format(indicator, missing_date)
+                        indicator_report = QuantityIndicatorReportFactory(
+                            reportable=indicator,
+                            time_period_start=missing_date,
+                            time_period_end=end_date,
+                        )
+
+                        for location in indicator.locations.all():
+                            print "Creating IndicatorLocationData object for {}".format(indicator_report)
+                            location_data = IndicatorLocationDataFactory(
+                                indicator_report=indicator_report,
+                                location=location,
+                                num_disaggregation=indicator_report.disaggregations.count(),
+                                level_reported=indicator_report.disaggregations.count(),
+                                disaggregation_reported_on=indicator_report.disaggregations.values_list(
+                                    'id', flat=True),
+                                disaggregation={}
+                            )
+
+                    else:
+                        print "Creating Ratio IndicatorReport object for {} - {}".format(indicator, missing_date)
+                        indicator_report = RatioIndicatorReportFactory(
+                            reportable=indicator,
+                            time_period_start=missing_date,
+                            time_period_end=end_date,
+                        )
+
+                        for location in indicator.locations.all():
+                            print "Creating IndicatorLocationData object {} - {}".format(indicator_report, missing_date)
+                            location_data = IndicatorLocationDataFactory(
+                                indicator_report=indicator_report,
+                                location=location,
+                                num_disaggregation=indicator_report.disaggregations.count(),
+                                level_reported=indicator_report.disaggregations.count(),
+                                disaggregation_reported_on=indicator_report.disaggregations.values_list(
+                                    'id', flat=True),
+                                disaggregation={}
+                            )
+
+                    indicator_report.progress_report = progress_report
+                    indicator_report.save()
