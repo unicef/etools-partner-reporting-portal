db1f173b49073317d0864d98fbdcb06cb9a826b5
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-07-26 11:17:08 +0200

    fix test simple location

--- django_api/django_api/apps/core/urls.py
+++ django_api/django_api/apps/core/urls.py
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
     url(r'^simple-intervention/$', SimpleInterventionAPIView.as_view(), name="simple-intervention"),
-    url(r'^^(?P<response_plan_id>\d+)/simple-location/$', SimpleLocationListAPIView.as_view(), name="simple-location"),
+    url(r'^(?P<response_plan_id>\d+)/simple-location/$', SimpleLocationListAPIView.as_view(), name="simple-location"),
     url(r'^(?P<location_id>\d+)/children-location/$', ChildrenLocationAPIView.as_view(), name="children-location"),
     url(r'^(?P<intervention_id>\d+)/response-plan/$', ResponsePlanAPIView.as_view(), name="response-plan"),
 ]
