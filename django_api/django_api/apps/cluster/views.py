e3d63adeeec451ef18a90620ddff7f5ef026745e
Leszek Kołacz <leszek.kolacz@tivix.com>
2017-07-28 09:17:31 +0200

    rm unused import

--- django_api/django_api/apps/cluster/views.py
+++ django_api/django_api/apps/cluster/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework import status as statuses, serializers
 
 import django_filters
