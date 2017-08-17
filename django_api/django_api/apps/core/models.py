a3ba7ad15dc396c1acec8a232aedcff6682abb67
Silin Na <n1207n@gmail.com>
2017-08-17 14:18:55 -0700

    refactor: Fixed the migration merges and created CartoDBTable and GatewayType models [#265]

--- django_api/django_api/apps/core/models.py
+++ django_api/django_api/apps/core/models.py
@@ -1,10 +1,25 @@
 from __future__ import unicode_literals
+import random
+import logging
 from decimal import Decimal
 
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db import models
+from django.core.cache import cache
+from django.contrib.gis.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator
+)
+from django.db import (
+    IntegrityError,
+    connection
+)
+from django.db.models.signals import post_delete, post_save
+from django.dispatch.dispatcher import receiver
 
 from model_utils.models import TimeStampedModel
+from mptt.models import MPTTModel, TreeForeignKey
+from paintstore.fields import ColorPickerField
 
 from .common import (
     INTERVENTION_TYPES,
@@ -12,6 +27,13 @@ from .common import (
     RESPONSE_PLAN_TYPE,
 )
 from .countries import COUNTRIES_ALPHA2_CODE_DICT, COUNTRIES_ALPHA2_CODE
+
+logger = logging.getLogger('locations.models')
+
+
+def get_random_color():
+    r = lambda: random.randint(0,255)
+    return '#%02X%02X%02X' % (r(), r(), r())
 
 
 class Intervention(TimeStampedModel):
@@ -115,17 +137,44 @@ class ResponsePlan(TimeStampedModel):
         return []  # TODO probably create file field
 
 
+class GatewayType(models.Model):
+    """
+    Represents an Admin Type in location-related models.
+    """
+
+    name = models.CharField(max_length=64L, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Location Type'
+
+    def __unicode__(self):
+        return self.name
+
+
+class LocationManager(models.GeoManager):
+
+    def get_queryset(self):
+        return super(LocationManager, self).get_queryset().select_related('gateway')
+
+
 class Location(TimeStampedModel):
     """
     Location model define place where agents are working.
     The background of the location can be: Country > Region > City > District/Point.
 
+    Either a point or geospatial object.
+    pcode should be unique.
+
     related models:
         indicator.Reportable (ForeignKey): "reportable"
         core.Location (ForeignKey): "self"
+        core.GatewayType: "gateway"
     """
     title = models.CharField(max_length=255)
     reportable = models.ForeignKey('indicator.Reportable', null=True, blank=True, related_name="locations")
+
+    gateway = models.ForeignKey(GatewayType, verbose_name='Location Type')
 
     latitude = models.DecimalField(
         null=True,
@@ -144,9 +193,10 @@ class Location(TimeStampedModel):
     p_code = models.CharField(max_length=32, blank=True, null=True)
 
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
-    # uncomment when dj will be >= 1.11
-    # geom = models.MultiPolygonField(null=True, blank=True)
-    # point = models.PointField(null=True, blank=True)
+
+    geom = models.MultiPolygonField(null=True, blank=True)
+    point = models.PointField(null=True, blank=True)
+    objects = LocationManager()
 
     class Meta:
         unique_together = ('title', 'p_code')
@@ -154,7 +204,14 @@ class Location(TimeStampedModel):
 
     def __str__(self):
         if self.p_code:
-            return "%s {PCode: %s}" % (self.title, self.p_code)
+            return '{} ({} {})'.format(
+                self.name,
+                self.gateway.name,
+                "{}: {}".format(
+                    'CERD' if self.gateway.name == 'School' else 'PCode',
+                    self.p_code if self.p_code else ''
+                    ))
+
         return self.title
 
     @property
@@ -167,3 +224,35 @@ class Location(TimeStampedModel):
             self.point.y,
             self.point.x
         )
+
+
+@receiver(post_delete, sender=Location)
+@receiver(post_save, sender=Location)
+def invalidate_locations_etag(sender, instance, **kwargs):
+    """
+    Invalidate the locations etag in the cache on every change.
+    """
+    schema_name = connection.schema_name
+    cache.delete("{}-locations-etag".format(schema_name))
+
+
+class CartoDBTable(MPTTModel):
+    """
+    Represents a table in CartoDB, it is used to import lacations
+    related models:
+        core.GatewayType: 'gateway'
+    """
+
+    domain = models.CharField(max_length=254)
+    api_key = models.CharField(max_length=254)
+    table_name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+    location_type = models.ForeignKey(GatewayType)
+    name_col = models.CharField(max_length=254, default='name')
+    pcode_col = models.CharField(max_length=254, default='pcode')
+    parent_code_col = models.CharField(max_length=254, null=True, blank=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    color = ColorPickerField(null=True, blank=True, default=get_random_color)
+
+    def __unicode__(self):
+        return self.table_name
