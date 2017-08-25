563f49aaf603e1fc4111bc7c367eb238d8356e04
dfryta <dariuszfryta@gmail.com>
2017-08-25 21:53:17 +0200

    Added first 12 columns

--- django_api/django_api/apps/cluster/export.py
+++ django_api/django_api/apps/cluster/export.py
@@ -18,8 +18,45 @@ class XLSXWriter:
 
         for indicator in self.indicators:
 
-            self.sheet.cell(row=row_id, column=1).value = indicator.reportable.cluster_objectives.first().cluster.title if indicator.reportable.cluster_objectives.first() else ""
-            # self.sheet.cell(row=row_id, column=1).value = ""
+            cluster_objective = indicator.reportable.cluster_objectives.first()
+            cluster_activity = indicator.reportable.cluster_activities.first()
+            partner_project = indicator.reportable.partner_projects.first()
+            partner_activity = indicator.reportable.partner_activities.first()
+
+            cluster = None
+
+            if cluster_objective:
+                cluster = cluster_objective.cluster
+                partner_activity = cluster_objective.cluster_activities.first().partner_activities.first()
+                partner_project = cluster_objective.cluster.partner_projects.first()
+            elif cluster_activity:
+                cluster = cluster_activity.cluster_objective.cluster
+                cluster_objective = cluster_activity.cluster_objective
+                partner_activity = cluster_activity.partner_activities.first()
+                partner_project = cluster_activity.cluster_objective.cluster.partner_projects.first()
+            elif partner_activity:
+                cluster = partner_activity.cluster_activity.cluster_objective.cluster
+                cluster_objective = partner_activity.cluster_activity.cluster_objective
+                partner_project = partner_activity.project
+            elif partner_project:
+                cluster = partner_project.clusters.first()
+                cluster_objective = partner_project.clusters.first().cluster_objectives.first()
+                partner_activity = partner_project.partner.partner_activities.first()
+
+
+            self.sheet.cell(row=row_id, column=1).value = cluster.title
+            self.sheet.cell(row=row_id, column=2).value = cluster.partner_projects.first().partner.title
+            self.sheet.cell(row=row_id, column=3).value = cluster_objective.title
+            self.sheet.cell(row=row_id, column=4).value = partner_activity.title
+            self.sheet.cell(row=row_id, column=5).value = indicator.reportable.blueprint.title
+            self.sheet.cell(row=row_id, column=6).value = partner_project.title
+            self.sheet.cell(row=row_id, column=7).value = partner_project.get_status_display()
+            self.sheet.cell(row=row_id, column=8).value = partner_activity.start_date
+            self.sheet.cell(row=row_id, column=9).value = partner_activity.end_date
+
+            self.sheet.cell(row=row_id, column=10).value = indicator.reportable.baseline
+            self.sheet.cell(row=row_id, column=11).value = indicator.reportable.target
+            self.sheet.cell(row=row_id, column=12).value = indicator.reportable.total['c']
 
             self.sheet.cell(row=row_id, column=16).value = indicator.time_period_start
             self.sheet.cell(row=row_id, column=17).value = indicator.time_period_end
