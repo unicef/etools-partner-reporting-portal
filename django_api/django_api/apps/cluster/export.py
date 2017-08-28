62a5237e8f62347ad037d141db8e9762fe754517
dfryta <dariuszfryta@gmail.com>
2017-08-28 12:38:04 +0200

    Added locations per row

--- django_api/django_api/apps/cluster/export.py
+++ django_api/django_api/apps/cluster/export.py
@@ -22,6 +22,7 @@ class XLSXWriter:
             cluster_activity = indicator.reportable.cluster_activities.first()
             partner_project = indicator.reportable.partner_projects.first()
             partner_activity = indicator.reportable.partner_activities.first()
+            locations_data = indicator.indicator_location_data
 
             cluster = None
 
@@ -43,26 +44,47 @@ class XLSXWriter:
                 cluster_objective = partner_project.clusters.first().cluster_objectives.first()
                 partner_activity = partner_project.partner.partner_activities.first()
 
+            # Each row is one location per indicator
 
-            self.sheet.cell(row=row_id, column=1).value = cluster.title
-            self.sheet.cell(row=row_id, column=2).value = cluster.partner_projects.first().partner.title
-            self.sheet.cell(row=row_id, column=3).value = cluster_objective.title
-            self.sheet.cell(row=row_id, column=4).value = partner_activity.title
-            self.sheet.cell(row=row_id, column=5).value = indicator.reportable.blueprint.title
-            self.sheet.cell(row=row_id, column=6).value = partner_project.title
-            self.sheet.cell(row=row_id, column=7).value = partner_project.get_status_display()
-            self.sheet.cell(row=row_id, column=8).value = partner_activity.start_date
-            self.sheet.cell(row=row_id, column=9).value = partner_activity.end_date
+            MOVE_COLUMN = 0
 
-            self.sheet.cell(row=row_id, column=10).value = indicator.reportable.baseline
-            self.sheet.cell(row=row_id, column=11).value = indicator.reportable.target
-            self.sheet.cell(row=row_id, column=12).value = indicator.reportable.total['c']
+            for location_data in locations_data.all():
 
-            self.sheet.cell(row=row_id, column=16).value = indicator.time_period_start
-            self.sheet.cell(row=row_id, column=17).value = indicator.time_period_end
+                self.sheet.cell(row=row_id, column=1).value = cluster.title
+                self.sheet.cell(row=row_id, column=2).value = cluster.partner_projects.first().partner.title
+                self.sheet.cell(row=row_id, column=3).value = cluster_objective.title
+                self.sheet.cell(row=row_id, column=4).value = partner_activity.title
+                self.sheet.cell(row=row_id, column=5).value = indicator.reportable.blueprint.title
+                self.sheet.cell(row=row_id, column=6).value = partner_project.title
+                self.sheet.cell(row=row_id, column=7).value = partner_project.get_status_display()
+                self.sheet.cell(row=row_id, column=8).value = partner_activity.start_date
+                self.sheet.cell(row=row_id, column=9).value = partner_activity.end_date
+
+                self.sheet.cell(row=row_id, column=10).value = indicator.reportable.baseline
+                self.sheet.cell(row=row_id, column=11).value = indicator.reportable.target
+                self.sheet.cell(row=row_id, column=12).value = indicator.reportable.total['c']
+
+                self.sheet.cell(row=row_id, column=13).value =location_data.location.title
+                self.sheet.cell(row=row_id, column=14).value = location_data.location.p_code
+                self.sheet.cell(row=row_id, column=15).value = "XXX" # TODO: no field here!
+
+                self.sheet.cell(row=row_id, column=16).value = indicator.time_period_start
+                self.sheet.cell(row=row_id, column=17).value = indicator.time_period_end
+
+                self.sheet.cell(row=row_id, column=18).value = indicator.get_overall_status_display()
 
 
-            row_id += 1
+
+                self.sheet.cell(row=row_id, column=19 + MOVE_COLUMN).value = indicator.submission_date
+                self.sheet.cell(row=row_id, column=20 + MOVE_COLUMN).value = cluster.id
+                self.sheet.cell(row=row_id, column=21 + MOVE_COLUMN).value = cluster_objective.id
+                self.sheet.cell(row=row_id, column=22 + MOVE_COLUMN).value = partner_activity.id
+                self.sheet.cell(row=row_id, column=23 + MOVE_COLUMN).value = indicator.reportable.blueprint.id
+                self.sheet.cell(row=row_id, column=24 + MOVE_COLUMN).value = partner_project.partner.id
+                self.sheet.cell(row=row_id, column=25 + MOVE_COLUMN).value = partner_project.id
+
+
+                row_id += 1
 
         filepath = SAVE_PATH + 'export.xlsx'
         self.wb.save(filepath)
