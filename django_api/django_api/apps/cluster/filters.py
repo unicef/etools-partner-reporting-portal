91aa4ae31ad6503876c2579b8fa16b29831cdd30
dfryta <dariuszfryta@gmail.com>
2017-09-11 11:27:03 +0200

    Fixed Cluster filter in Cluster Results page

--- django_api/django_api/apps/cluster/filters.py
+++ django_api/django_api/apps/cluster/filters.py
@@ -74,7 +74,12 @@ class ClusterIndicatorsFilter(django_filters.FilterSet):
             return queryset.exclude(report_status=INDICATOR_REPORT_STATUS.submitted)
 
     def get_cluster(self, queryset, name, value):
-        return queryset.filter(reportable__cluster_objectives__cluster=value)
+        return queryset.filter(
+            Q(reportable__cluster_objectives__cluster=value) |
+            Q(reportable__cluster_activities__cluster_objective__cluster=value) |
+            Q(reportable__partner_activities__cluster_activity__cluster_objective__cluster=value) |
+            Q(reportable__partner_projects__clusters__id__exact=value)
+        )
 
     def get_partner(self, queryset, name, value):
         return queryset.filter(reportable__partner_projects__partner=value)
