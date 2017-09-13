e09cdda880737d8c69dd9d109e91e86fbce32976
Silin Na <n1207n@gmail.com>
2017-09-13 12:59:25 -0700

    feat: Added CartoDBTableAdmin using CartoDBTableForm and syncer method [#265]

--- django_api/django_api/apps/core/admin.py
+++ django_api/django_api/apps/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from core.forms import GatewayTypeModelForm
-from .models import (
+from core.forms import GatewayTypeModelForm, CartoDBTableForm
+from core.cartodb import update_sites_from_cartodb
+from core.models import (
     Intervention,
     Location,
     ResponsePlan,
@@ -15,8 +16,30 @@ class GatewayTypeAdmin(admin.ModelAdmin):
     fields = ('name', 'admin_level', 'intervention')
 
 
+class CartoDBTableAdmin(admin.ModelAdmin):
+    form = CartoDBTableForm
+    save_as = True
+    list_display = (
+        'domain',
+        'api_key',
+        'username',
+        'table_name',
+        'location_type',
+        'parent_table_name',
+    )
+
+    actions = ('import_sites',)
+
+    def parent_table_name(self, obj):
+        return obj.parent.table_name if obj.parent else "No parent"
+
+    def import_sites(self, request, queryset):
+        for table in queryset:
+            update_sites_from_cartodb.delay(table)
+
+
 admin.site.register(Intervention)
 admin.site.register(Location)
 admin.site.register(ResponsePlan)
 admin.site.register(GatewayType)
-admin.site.register(CartoDBTable)
+admin.site.register(CartoDBTable, CartoDBTableAdmin)
