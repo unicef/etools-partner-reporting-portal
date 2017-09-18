4d2e1f0032e4b6fc720af62a3f837a4f3c4d844d
dfryta <dariuszfryta@gmail.com>
2017-09-18 14:10:05 +0200

    Added filters to ProgrammeDocuments

--- django_api/django_api/apps/unicef/filters.py
+++ django_api/django_api/apps/unicef/filters.py
@@ -6,8 +6,6 @@ from distutils.util import strtobool
 from django_filters.filters import ChoiceFilter, CharFilter, DateFilter, TypedChoiceFilter
 
 from core.common import PD_LIST_REPORT_STATUS, PD_STATUS, PROGRESS_REPORT_STATUS
-from core.models import Location
-from indicator.models import IndicatorReport
 from .models import ProgrammeDocument, ProgressReport
 
 BOOLEAN_CHOICES = (('0', 'False'), ('1', 'True'),)
@@ -17,8 +15,6 @@ class ProgrammeDocumentFilter(django_filters.FilterSet):
     ref_title = CharFilter(method='get_reference_number_title')
     status = ChoiceFilter(choices=PD_STATUS)
     location = CharFilter(method='get_location')
-    # report_status = ChoiceFilter(choices=PD_LIST_REPORT_STATUS, method='get_report_status')
-    # due_date = DateFilter(method='get_due_date')
 
     class Meta:
         model = ProgrammeDocument
@@ -29,35 +25,11 @@ class ProgrammeDocumentFilter(django_filters.FilterSet):
             Q(reference_number__icontains=value) | Q(title__icontains=value)
         )
 
-    def get_location(self, queryset, name, value):
-        pd_ids = Location.objects.filter(
-            Q(id=value) |
-            Q(parent_id=value) |
-            Q(parent__parent_id=value) |
-            Q(parent__parent__parent_id=value) |
-            Q(parent__parent__parent__parent_id=value)
-        ).values_list(
-             'reportable__lower_level_outputs__cp_output__programme_document__id',
-             flat=True
-        )
-        return queryset.filter(pk__in=pd_ids)
+    def get_status(self, queryset, name, value):
+        return queryset.filter(status=value)
 
-    # def get_report_status(self, queryset, name, value):
-    #     # reports not exists OR property contain_nothing_due_report
-    #     if value == PD_LIST_REPORT_STATUS.nothing_due:
-    #         return queryset.filter(
-    #
-    #         )
-    #     # property contain_overdue_report
-    #     elif value == PD_LIST_REPORT_STATUS.overdue:
-    #         return queryset.filter(
-    #
-    #         )
-    #     # value == PD_LIST_REPORT_STATUS.due:
-    #     else:
-    #         return queryset.filter(
-    #
-    #         )
+    def get_location(self, queryset, name, value):
+        return queryset.filter(progress_reports__indicator_reports__indicator_location_data__location=value)
 
 
 class ProgressReportFilter(django_filters.FilterSet):
