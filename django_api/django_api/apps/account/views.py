e9b56353e97eadf58203694b23ecc02b0464fbfd
Sumit Chachra <sumit@tivix.com>
2017-10-02 13:09:02 +0100

    [#26] Added endpoints for retrieving user profile and logout.

--- django_api/django_api/apps/account/views.py
+++ django_api/django_api/apps/account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import status as statuses
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.permissions import IsAuthenticated
+
+from .models import UserProfile
+from .serializers import UserSerializer
+
+
+class UserProfileAPIView(RetrieveAPIView):
+    """
+    Returns the users profile details like name, id's, groups etc.
+    """
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data, status=statuses.HTTP_200_OK)
+
+
+class UserLogoutAPIView(APIView):
+    """
+    Simply logs out the user on POST request.
+    """
+    # permission_classes = (IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return Response({}, status=statuses.HTTP_200_OK)
