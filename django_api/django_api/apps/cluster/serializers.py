4d22e424adf02984509de30dbe997fda8cfb40ac
dfryta <dariuszfryta@gmail.com>
2017-10-12 13:09:38 +0200

    Added frequency_name to ClusterActivitySerializer

--- django_api/django_api/apps/cluster/serializers.py
+++ django_api/django_api/apps/cluster/serializers.py
@@ -69,6 +69,7 @@ class ClusterActivitySerializer(serializers.ModelSerializer):
     co_cluster_title = serializers.SerializerMethodField()
     co_title = serializers.SerializerMethodField()
     co_reference_number = serializers.SerializerMethodField()
+    frequency_name = serializers.SerializerMethodField()
 
     class Meta:
         model = ClusterActivity
@@ -81,6 +82,7 @@ class ClusterActivitySerializer(serializers.ModelSerializer):
             'co_title',
             'co_reference_number',
             'frequency',
+            'frequency_name',
             'cluster_objective',
         )
 
@@ -92,6 +94,9 @@ class ClusterActivitySerializer(serializers.ModelSerializer):
 
     def get_co_reference_number(self, obj):
         return obj.cluster_objective.reference_number
+
+    def get_frequency_name(self, obj):
+        return obj.get_frequency_display()
 
 
 class ClusterActivityPatchSerializer(serializers.ModelSerializer):
