59bfdf6dcf5ba3f6871fd72012981c7d7f99a64b
Sumit Chachra <sumit@tivix.com>
2017-10-12 18:30:23 -0700

    Return workspaces and imo_clusters in the api response for profile, added logic to return only workspaces associated with user. [#360]

--- django_api/django_api/apps/account/serializers.py
+++ django_api/django_api/apps/account/serializers.py
@@ -7,5 +7,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'profile',
-                  'groups', 'partner', 'organization')
+                  'groups', 'partner', 'organization', 'workspaces',
+                  'imo_clusters')
         depth = 1
