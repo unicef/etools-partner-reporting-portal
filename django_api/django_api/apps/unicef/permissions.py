e4c01bc662890a56ae2e2f30e3a631366e4f773e
robertavram <robert.avram@gmail.com>
2017-10-30 16:21:11 -0400

    Refactor permissions

--- django_api/django_api/apps/unicef/permissions.py
+++ django_api/django_api/apps/unicef/permissions.py
@@ -40,8 +40,7 @@ class UnicefPartnershipManagerOrRead(BasePermission):
         if user.is_authenticated() and request.method in SAFE_METHODS:
             return True
 
-        return user.is_authenticated() and \
-            hasattr(user, 'jwt_payload') and \
-            "groups" in user.jwt_payload and \
-            "Partnership Manager" in user.jwt_payload['groups'] or \
-            user.is_superuser
+        return (user.is_authenticated() and user.is_superuser) or \
+            (user.is_authenticated and hasattr(user, 'jwt_payload') and
+             hasattr(user, 'jwt_payload') and "groups" in user.jwt_payload and
+             "Partnership Manager" in user.jwt_payload['groups'])
