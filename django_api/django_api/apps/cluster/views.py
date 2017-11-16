d3b1964ad1967e680ff048a17ec9333378aac079
eglital <eglelibby@gmail.com>
2017-11-15 18:28:37 -0800

    sortable objective and response parameter activities

--- django_api/django_api/apps/cluster/views.py
+++ django_api/django_api/apps/cluster/views.py
@@ -173,8 +173,17 @@ class ClusterObjectiveListCreateAPIView(ListCreateAPIView):
     def get_queryset(self, *args, **kwargs):
         response_plan_id = self.kwargs.get('response_plan_id')
 
-        return ClusterObjective.objects.select_related('cluster').filter(
+        queryset = ClusterObjective.objects.select_related('cluster').filter(
             cluster__response_plan_id=response_plan_id)
+        order = self.request.query_params.get('sort', None)
+        if order:
+            order_field = order.split('.')[0]
+        if order and order_field in ('title', 'cluster'):
+            if order.split('.')[1] == 'asc':
+                queryset = queryset.order_by(order_field)
+            else:
+                queryset = queryset.order_by('-' + order_field)
+        return queryset
 
     def post(self, request, *args, **kwargs):
         """
@@ -281,8 +290,18 @@ class ClusterActivityListAPIView(ListCreateAPIView):
     def get_queryset(self, *args, **kwargs):
         response_plan_id = self.kwargs.get('response_plan_id')
 
-        return ClusterActivity.objects.select_related('cluster_objective__cluster').filter(
+        queryset = ClusterActivity.objects.select_related('cluster_objective__cluster').filter(
             cluster_objective__cluster__response_plan_id=response_plan_id)
+        order = self.request.query_params.get('sort', None)
+        if order:
+            order_field = order.split('.')[0]
+        if order and order_field in ('title', 'cluster_objective'):
+            if order.split('.')[1] == 'asc':
+                queryset = queryset.order_by(order_field)
+            else:
+                queryset = queryset.order_by('-' + order_field)
+
+        return queryset
 
     def post(self, request, *args, **kwargs):
         """
