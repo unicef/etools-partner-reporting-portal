a34671fea27b6a949960971145a78c2ab4757ce2
Silin Na <n1207n@gmail.com>
2018-01-19 14:59:41 -0800

    fix: Added missing import in urlpatterns [#377]

--- django_api/django_api/urls.py
+++ django_api/django_api/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 from rest_framework_swagger.views import get_swagger_view
 
