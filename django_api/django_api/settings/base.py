a73e4fe236f266937e7c7efa16adb8abe711f1d9
Silin Na <n1207n@gmail.com>
2018-01-24 11:10:01 -0800

    feat: Added rest_framework_gis django module [#436]
    + PyPI requirements update

--- django_api/django_api/settings/base.py
+++ django_api/django_api/settings/base.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+    'rest_framework_gis',
     'drfpasswordless',
     'django_filters',
     'djcelery',
