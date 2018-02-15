56c71e55d7083f7926013ffa2a9af3b0e8c26215
Silin Na <n1207n@gmail.com>
2018-02-15 15:00:33 -0800

    refactor: Renamed the email handler [#462]

--- django_api/django_api/apps/utils/emails.py
+++ django_api/django_api/apps/utils/emails.py
@@ -3,7 +3,7 @@ from django.template.loader import get_template
 from django.template import Context
 
 
-def send_email(subject, template_path, template_data, from_email, to_email_list, fail_silently=True):
+def send_email_from_template(subject, template_path, template_data, from_email, to_email_list, fail_silently=True):
     send_mail(
         subject,
         get_template(template_path).render(
