2e08425116a02e4530c45800bcacee0d0ecfce42
Silin Na <n1207n@gmail.com>
2018-02-15 14:59:37 -0800

    feat: Created emails utility method [#462]
    + send_email_from_template

---
+++ django_api/django_api/apps/utils/emails.py
@@ -0,0 +1,15 @@
+from django.core.mail import send_mail
+from django.template.loader import get_template
+from django.template import Context
+
+
+def send_email(subject, template_path, template_data, from_email, to_email_list, fail_silently=True):
+    send_mail(
+        subject,
+        get_template(template_path).render(
+            Context(template_data)
+        ),
+        from_email,
+        to_email_list,
+        fail_silently=fail_silently,
+    )
