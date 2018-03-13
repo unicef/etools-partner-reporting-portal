493fca2eac95b0cd86de5074e48b5fee1cc507d6
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-03-13 11:22:55 +0000

    lint

--- django_api/django_api/apps/indicator/serializers.py
+++ django_api/django_api/apps/indicator/serializers.py
@@ -27,7 +27,6 @@ from .models import (
     Reportable, IndicatorBlueprint,
     IndicatorReport, IndicatorLocationData,
     Disaggregation, DisaggregationValue,
-    get_reportable_data_to_clone,
     create_pa_reportables_for_new_ca_reportable,
 )
 
