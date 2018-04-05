c1eff3d90ccdac908b2face6330e1f6d04aae5d6
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-04-05 12:28:54 +0200

    S3 setup work cont'd

--- django_api/django_api/settings/base.py
+++ django_api/django_api/settings/base.py
@@ -421,6 +421,8 @@ if all([AZURE_ACCOUNT_NAME, AZURE_ACCOUNT_KEY, AZURE_CONTAINER]):
 elif all([AWS_S3_ACCESS_KEY_ID, AWS_S3_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET_NAME]):
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
+    AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-central-1')
 
 
 # JWT Authentication
