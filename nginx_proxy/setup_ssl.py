b34234c4e8fe9fdea2678e079f3e2431a4763b47
Maciej Jaworski <maciej.jaworski@tivix.com>
2018-04-25 13:49:29 +0200

    Add cronjob to renew certificates

--- nginx_proxy/setup_ssl.py
+++ nginx_proxy/setup_ssl.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from crontab import CronTab
 
 DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 ADMIN_MAIL = os.getenv('ADMIN_MAIL')
@@ -21,14 +22,14 @@ if __name__ == '__main__':
     if not DOMAIN_NAME and not ADMIN_MAIL:
         raise Exception('Cannot proceed without DOMAIN_NAME and ADMIN_MAIL')
 
-    if not os.path.isfile(CERTIFICATE_FILE):
-        default_args = [
-            '--cert-path', WORKDIR,
-            '--config-dir', WORKDIR,
-            '--work-dir', WORKDIR,
-        ]
+    default_args = [
+        '--cert-path', WORKDIR,
+        '--config-dir', WORKDIR,
+        '--work-dir', WORKDIR,
+    ]
 
-        full_args = [
+    if not os.path.isfile(CERTIFICATE_FILE):
+        get_cert_args = [
             'certbot',
             'certonly',
             '--webroot',
@@ -39,7 +40,7 @@ if __name__ == '__main__':
             '--webroot-path', '/usr/share/nginx/html',
         ] + default_args
 
-        certificate_call_status = subprocess.call(full_args)
+        certificate_call_status = subprocess.call(get_cert_args)
 
         if certificate_call_status == 0:
             raise Exception('Could not setup certificate')
@@ -50,10 +51,18 @@ if __name__ == '__main__':
             '/nginx-site.conf.j2'
         ], env=dict(
             os.environ,
-            NGINX_LISTEN_PORT=str(443),
             SSL_CERTIFICATE=CERTIFICATES_PATH
         ), stdout=nginx_ssl_config)
 
     if not ssl_config_render_call_status == 0:
-        os.remove(NGINX_SSL_CFG_FILE)
-        raise Exception('Error rendering Nginx SSL config from template')
+        with open(NGINX_SSL_CFG_FILE, "w") as nginx_ssl_config:
+            ssl_config_render_call_status = subprocess.call([
+                'j2',
+                '/nginx-site.conf.j2'
+            ], stdout=nginx_ssl_config)
+
+    cron = CronTab(user=True)
+    renew_job = cron.new(command=' '.join(
+        ['certbot', 'renew'] + default_args
+    ))
+    renew_job.hour.on(0)
