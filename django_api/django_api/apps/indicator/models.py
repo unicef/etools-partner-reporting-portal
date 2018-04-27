7e7e7022d06c40ee4cab40677237c75c7ae98160
Silin Na <n1207n@gmail.com>
2018-04-27 14:12:23 -0700

    feat: Moved cluster_activity_indicator_id to Reportable from PD [#629]
    + Adjusted DB migrations

--- django_api/django_api/apps/indicator/models.py
+++ django_api/django_api/apps/indicator/models.py
@@ -235,6 +235,13 @@ class Reportable(TimeStampedExternalSourceModel):
     start_date_of_reporting_period = models.DateField(blank=True, null=True)
 
     is_cluster_indicator = models.BooleanField(default=False)
+    cluster_activity_indicator_id = models.CharField(
+        help_text='An ID representing ClusterActivity Reportable in PRP for dual reporting',
+        blank=True,
+        null=True,
+        max_length=32
+    )
+
     contributes_to_partner = models.BooleanField(default=False)
 
     # Current total, transactional and dynamically calculated based on
