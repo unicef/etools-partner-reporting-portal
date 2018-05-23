c69507a80778e4b09755f0e0adebc246f4a58e8c
dfryta <dariuszfryta@gmail.com>
2018-05-23 23:46:54 +0200

    Added new partner endpoint. Added new fields. Added country. Removed unused fields. Increased city to 64 chars. Updated partners sync.

--- django_api/django_api/apps/partner/tasks.py
+++ django_api/django_api/apps/partner/tasks.py
@@ -1,32 +1,82 @@
+import requests
+
 from celery import shared_task
 from core.api import PMP_API
+from core.models import Workspace
+from core.countries import COUNTRIES_ALPHA2_CODE_DICT
 
 from partner.serializers import PMPPartnerSerializer
 from partner.models import Partner
 
+MAX_RETRIES = 6
 
 @shared_task
-def process_partners():
+def process_partners(area=None):
     # Hit API
     api = PMP_API()
-    data = api.partners()
 
-    # Create partners
-    try:
-        for item in data:
-            if item['hidden']:
-                continue
-            print("Creating Partner: %s" % item['vendor_number'])
-            try:
-                instance = Partner.objects.get(
-                    vendor_number=item['vendor_number'])
-                serializer = PMPPartnerSerializer(instance, data=item)
-            except Partner.DoesNotExist:
-                serializer = PMPPartnerSerializer(data=item)
-            if serializer.is_valid():
-                serializer.save()
-            else:
-                raise Exception(serializer.errors)
-    except Exception as e:
-        print(e)
-        raise
+
+    if area:
+        workspaces = Workspace.objects.filter(business_area_code=area)  # 2130 for Iraq
+    else:
+        workspaces = Workspace.objects.all()
+
+
+
+    for workspace in workspaces:
+        # Skip global workspace and Syria Cross Border / MENARO
+        if workspace.business_area_code in ("0", "234R"):
+            continue
+        # Create partners
+        try:
+            # Iterate over all pages
+            page_url = None
+            retries = 0
+            while True:
+                try:
+                    data = api.partners(business_area_code=workspace.business_area_code, url=page_url)
+                    retries = 0
+                except Exception as e:
+                    retries += 1
+                    print(e)
+                    if retries < MAX_RETRIES:
+                        print("TIMEOUT! Retry {}".format(retries))
+                        continue
+                    else:
+                        print("Skip country!")
+                        break
+
+                for item in data['results']:
+                    # Fill missing country code value
+                    # We have M2M, but PMP API return 1-to-1
+                    # In addition we have to save 2-alpha code
+                    country = workspace.countries.first().details
+                    item['country_code'] = country.alpha_2 if country else None
+
+                    print("{} Creating Partner: {}".format(workspace.workspace_code, item['unicef_vendor_number']))
+                    try:
+                        if not item['unicef_vendor_number']:
+                            print("\tNo unicef_vendor_number - SKIPPING!")
+                            continue
+                        if not item['name']:
+                            print("\tNo Partner name provided - SKIPPING!")
+                            continue
+                        instance = Partner.objects.get(
+                            vendor_number=item['unicef_vendor_number'])
+                        serializer = PMPPartnerSerializer(instance, data=item)
+                    except Partner.DoesNotExist:
+                        serializer = PMPPartnerSerializer(data=item)
+                    if serializer.is_valid():
+                        serializer.save()
+                    else:
+                        raise Exception(serializer.errors)
+                        # Check if another page exists
+                if data['next']:
+                    print("Found new page")
+                    page_url = data['next']
+                else:
+                    print("End of workspace")
+                    break
+        except Exception as e:
+            print(e)
+            raise
