36e0d8ae6d91236df68c6b31c3589a8fa88203b4
Dariusz Fryta <dariuszfryta@MacBook-Pro-3.local>
2018-07-27 11:41:40 +0200

    #805 - fixing cluster objective edit

--- django_api/django_api/apps/cluster/serializers.py
+++ django_api/django_api/apps/cluster/serializers.py
@@ -58,7 +58,7 @@ class ClusterObjectiveSerializer(serializers.ModelSerializer):
         return data
 
 
-class ClusterObjectivePatchSerializer(ClusterObjectiveSerializer):
+class ClusterObjectivePatchSerializer(serializers.ModelSerializer):
     title = serializers.CharField(required=False)
 
     class Meta:
