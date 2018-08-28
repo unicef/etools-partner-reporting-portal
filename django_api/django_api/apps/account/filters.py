7267977e527300ea4ed82027450dcecd81b6b203
Krzysztof Kosiorek <krzysztof.kosiorek@tivix.com>
2018-08-28 11:31:04 +0200

    add status filter for user list

--- django_api/django_api/apps/account/filters.py
+++ django_api/django_api/apps/account/filters.py
@@ -1,9 +1,14 @@
+import operator
+from functools import reduce
+from urllib import parse
+
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
 
 import django_filters
 from django_filters.filters import CharFilter
 
+from core.common import USER_STATUS_TYPES
 from utils.filters.fields import CommaSeparatedListFilter
 
 from .models import User
@@ -11,6 +16,7 @@ from .models import User
 
 class UserFilter(django_filters.FilterSet):
     name_email = CharFilter(method='get_name_email')
+    status = CharFilter(method='get_status')
     roles = CommaSeparatedListFilter(name='prp_roles__role')
     partners = CommaSeparatedListFilter(name='partner_id')
     clusters = CommaSeparatedListFilter(name='prp_roles__cluster_id')
@@ -23,3 +29,14 @@ class UserFilter(django_filters.FilterSet):
     def get_name_email(self, queryset, name, value):
         queryset = queryset.annotate(name=Concat('first_name', Value(' '), 'last_name'))
         return queryset.filter(Q(name__icontains=value) | Q(email__icontains=value))
+
+    def get_status(self, queryset, name, value):
+        statuses = parse.unquote(value).split(',')
+        status_to_q = {
+            USER_STATUS_TYPES.active: Q(is_active__isnull=False, last_login__isnull=False),
+            USER_STATUS_TYPES.invited: Q(is_active__isnull=False, last_login__isnull=True),
+            USER_STATUS_TYPES.incomplete: Q(prp_roles__isnull=True)
+        }
+        return queryset.filter(reduce(
+            operator.or_, [status_to_q.get(status, Q()) for status in statuses]
+        ))
