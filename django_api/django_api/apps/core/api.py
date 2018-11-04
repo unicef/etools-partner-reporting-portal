17576cda46db3f63050c62d8e3b554dda6605c10
robertavram <robert.avram@gmail.com>
2018-11-04 13:26:22 +0200

    Increase request timeouts

--- django_api/django_api/apps/core/api.py
+++ django_api/django_api/apps/core/api.py
@@ -79,24 +79,24 @@ class PMP_API(object):
 
     def workspaces(self):
         self.url = self.url_prototype + "/v2/workspaces/"
-        data = self._push_request(timeout=30)
+        data = self._push_request(timeout=300)
         return data
 
     def programme_documents(self, business_area_code, page=0, url=None):
         self.url = self.url_prototype + \
             "/prp/v1/interventions?page=%s&limit=10&workspace=%s" % (
                 page, business_area_code) if not url else url
-        data = self._push_request(timeout=30)
+        data = self._push_request(timeout=300)
         return data
 
     def partners(self, business_area_code, url=None):
         self.url = self.url_prototype + "/prp/v1/partners/?workspace=%s" % (
                 business_area_code) if not url else url
-        data = self._push_request(timeout=30)
+        data = self._push_request(timeout=300)
         return data
 
     def get_pd_document_url(self, business_area_code, pd_external_id):
         self.url = self.url_prototype + "/prp/v1/get_pd_document/{}/?workspace={}".format(pd_external_id,
                                                                                           business_area_code)
-        data = self._simple_get_request(timeout=30)
+        data = self._simple_get_request(timeout=300)
         return data
