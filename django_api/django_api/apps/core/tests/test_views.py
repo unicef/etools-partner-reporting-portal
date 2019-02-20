c3a8ba10c3b04ccc99d3962182c101177bd94b77
Silin Na <n1207n@gmail.com>
2019-02-19 16:18:39 -0800

    test: Fixed test cases for functional combinatorics tests and Disaggregators [#592]
    + TestCombinatorics
    
    + TestQuantityIndicatorDisaggregator
    
    + TestRatioIndicatorDisaggregator

--- django_api/django_api/apps/core/tests/test_views.py
+++ django_api/django_api/apps/core/tests/test_views.py
@@ -1,69 +1,48 @@
 from django.urls import reverse
 
-# from rest_framework imports status
-
-# from core.models imports ResponsePlan
-# from core.models imports Location, Workspace
 from rest_framework import status
 
 from core.common import CLUSTER_TYPES, PRP_ROLE_TYPES
-from core.models import Workspace
-from core.factories import PRPRoleFactory
+from core.models import Workspace, ResponsePlan, Location
+from core.factories import ClusterPRPRoleFactory
 from core.tests.base import BaseAPITestCase
 
 
-# class TestWorkspaceListAPIView(BaseAPITestCase):
+class TestWorkspaceListAPIView(BaseAPITestCase):
 
-#     def test_list_api(self):
-#         url = reverse('workspace')
-#         response = self.client.get(url, format='json')
+    def test_list_api(self):
+        url = reverse('workspace')
+        response = self.client.get(url, format='json')
 
-#         self.assertEquals(response.status_code, status.HTTP_200_OK)
-#         self.assertEquals(
-#             len(response.data),
-#             Workspace.objects.prefetch_related('locations').filter(
-#                 locations__isnull=False).distinct().count()
-#         )
-
-
-# class TestLocationListAPIView(BaseAPITestCase):
-
-#     def test_list_api(self):
-#         response_plan_id = ResponsePlan.objects.first().id
-#         url = reverse(
-#             'location', kwargs={
-#                 'response_plan_id': response_plan_id})
-#         response = self.client.get(url, format='json')
-
-#         result = ResponsePlan.objects.filter(id=response_plan_id).values_list(
-#             'clusters__cluster_objectives__reportables__locations',
-#             'clusters__cluster_objectives__cluster_activities__reportables__locations',
-#             'clusters__partner_projects__reportables__locations',
-#             'clusters__partner_projects__partner_activities__reportables__locations',
-#         ).distinct()
-#         pks = []
-#         [pks.extend(filter(lambda x: x is not None, part)) for part in result]
-#         expected = Location.objects.filter(pk__in=pks).count()
-
-#         self.assertEquals(response.status_code, status.HTTP_200_OK)
-#         self.assertEquals(len(response.data), expected)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(
+            len(response.data),
+            Workspace.objects.prefetch_related('locations').filter(
+                locations__isnull=False).distinct().count()
+        )
 
 
-# class TestResponsePlanAPIView(BaseAPITestCase):
+class TestLocationListAPIView(BaseAPITestCase):
 
-#     def test_response_plan(self):
-#         workspace = Workspace.objects.first()
-#         response_plan_count = ResponsePlan.objects.filter(
-#             workspace=workspace).count()
-#         url = reverse("response-plan", kwargs={'workspace_id': workspace.id})
-#         response = self.client.get(url, format='json')
+    def test_list_api(self):
+        response_plan_id = ResponsePlan.objects.first().id
+        url = reverse(
+            'location', kwargs={
+                'response_plan_id': response_plan_id})
+        response = self.client.get(url, format='json')
 
-#         self.assertEquals(response.status_code, status.HTTP_200_OK)
-#         self.assertEquals(len(response.data), response_plan_count)
-#         self.assertTrue(response.data[0].get('id') in
-#                         ResponsePlan.objects.filter(
-#                             workspace=workspace).values_list('id', flat=True))
-#         self.assertEquals(response.data[0].get('workspace'), workspace.id)
+        result = ResponsePlan.objects.filter(id=response_plan_id).values_list(
+            'clusters__cluster_objectives__reportables__locations',
+            'clusters__cluster_objectives__cluster_activities__reportables__locations',
+            'clusters__partner_projects__reportables__locations',
+            'clusters__partner_projects__partner_activities__reportables__locations',
+        ).distinct()
+        pks = []
+        [pks.extend(filter(lambda x: x is not None, part)) for part in result]
+        expected = Location.objects.filter(pk__in=pks).count()
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(len(response.data), expected)
 
 
 class TestResponsePlanAPIView(BaseAPITestCase):
@@ -72,7 +51,7 @@ class TestResponsePlanAPIView(BaseAPITestCase):
         super(TestResponsePlanAPIView, self).setUp()
         self.workspace = Workspace.objects.first()
 
-        PRPRoleFactory(
+        ClusterPRPRoleFactory(
             user=self.user,
             role=PRP_ROLE_TYPES.imo_cluster,
         )
