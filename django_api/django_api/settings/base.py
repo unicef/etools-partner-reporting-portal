c8a67d8287028561e513c20f59b03fc2560fd0f8
Silin Na <n1207n@gmail.com>
2019-02-28 15:24:55 -0800

    fix: Disabled create_user pipeline function in python-social-auth settings [#1078]

--- django_api/django_api/settings/base.py
+++ django_api/django_api/settings/base.py
@@ -445,7 +445,7 @@ SOCIAL_AUTH_PIPELINE = (
     # 'social_core.pipeline.user.get_username',
     'core.mixins.get_username',
     'social_core.pipeline.social_auth.associate_by_email',
-    'social_core.pipeline.user.create_user',
+    # 'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     # 'social_core.pipeline.user.user_details',
