7254225f6d6e3c0aad9477f57239eedd40765de9
Silin Na <n1207n@gmail.com>
2019-03-01 15:42:50 -0800

    feat: Created 2 celery task functions for sending email notification on due and overdue progress reports [#1074]
    + notify_ip_due_reports
    
    + notify_ip_overdue_reports
    
    + send_due_progress_report_email
    
    + send_overdue_progress_report_email
    
    + email templates

--- django_api/django_api/apps/indicator/tasks.py
+++ django_api/django_api/apps/indicator/tasks.py
@@ -1,11 +1,16 @@
+import logging
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
+
 from celery import shared_task
 
 from indicator.models import IndicatorReport
 from unicef.models import ProgressReport
 from core.common import INDICATOR_REPORT_STATUS, PROGRESS_REPORT_STATUS
+from utils.emails import send_due_progress_report_email, send_overdue_progress_report_email
 
-from datetime import datetime, timedelta
 
+logger = logging.getLogger(__name__)
 OVERDUE_DAYS = 15
 
 
@@ -13,12 +18,12 @@ OVERDUE_DAYS = 15
 def process_due_reports():
     updates = list()
     today = datetime.now().date()
-    print("Create due/overdue indicator reports")
+    logger.info("Create due/overdue indicator reports")
     # Get all open (without submission date) indicator reports
     reports = IndicatorReport.objects.filter(submission_date__isnull=True)
     # Iterate and set proper status
     for report in reports:
-        print("Indicator Report: %s" % report.id)
+        logger.info("Indicator Report: %s" % report.id)
         due_date = report.due_date or report.time_period_end + timedelta(days=OVERDUE_DAYS)
         if due_date < today and report.report_status != INDICATOR_REPORT_STATUS.overdue:
             report.report_status = INDICATOR_REPORT_STATUS.overdue
@@ -32,7 +37,7 @@ def process_due_reports():
     reports = ProgressReport.objects.filter(submission_date__isnull=True)
     # Iterate and set proper status
     for report in reports:
-        print("Progress Report: %s" % report.id)
+        logger.info("Progress Report: %s" % report.id)
         due_date = report.due_date or report.end_date + timedelta(days=OVERDUE_DAYS)
         if due_date < today and report.status != PROGRESS_REPORT_STATUS.overdue:
             report.status = PROGRESS_REPORT_STATUS.overdue
@@ -47,3 +52,39 @@ def process_due_reports():
 
     return "Updated %s Reports: %s" % (len(updates), ", ".join(["(%s for ID %d)" % (
         status, report.id) for status, report in updates])) if updates else "---"
+
+
+@shared_task
+def notify_ip_due_reports():
+    logger.info("Notifying IP due progress reports")
+    notified = list()
+
+    today = date.today()
+    unsubmitted_due_reports = ProgressReport.objects.filter(
+        submission_date__isnull=True,
+        status=PROGRESS_REPORT_STATUS.due,
+        due_date=today + relativedelta(days=7),
+    )
+
+    for report in unsubmitted_due_reports:
+        send_due_progress_report_email(report)
+        notified.append(report.id)
+
+    return "Sent emails for %s Due Report IDs: %s" % (len(notified), ", ".join(notified)) if notified else "---"
+
+
+@shared_task
+def notify_ip_overdue_reports():
+    logger.info("Notifying IP overdue progress reports")
+    notified = list()
+
+    unsubmitted_overdue_reports = ProgressReport.objects.filter(
+        submission_date__isnull=True,
+        status=PROGRESS_REPORT_STATUS.overdue,
+    )
+
+    for report in unsubmitted_overdue_reports:
+        send_overdue_progress_report_email(report)
+        notified.append(report.id)
+
+    return "Sent emails for %s Overdue Report IDs: %s" % (len(notified), ", ".join(notified)) if notified else "---"
--- django_api/django_api/apps/utils/emails.py
+++ django_api/django_api/apps/utils/emails.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
-from django.template.loader import get_template
+from django.template.loader import render_to_string
 
 
 def send_email_from_template(
@@ -29,11 +29,71 @@ def send_email_from_template(
     """
 
     message = EmailMultiAlternatives(
-        get_template(subject_template_path).render(template_data).strip(),
-        get_template(body_template_path).render(template_data),
+        render_to_string(subject_template_path, template_data).strip(),
+        render_to_string(body_template_path, template_data),
         from_email,
         to_email_list,
         **kwargs
     )
     message.content_subtype = content_subtype
     message.send(fail_silently=fail_silently)
+
+
+def send_due_progress_report_email(progress_report):
+    """send_due_progress_report_email sends email notifications to
+    UNICEF Authorized Officers and Focal Points about reports due in 1 week.
+
+    Arguments:
+        progress_report {ProgressReport} -- ProgressReport ORM object to send email notification about
+    """
+
+    pd = progress_report.programme_document
+
+    template_data = {
+        'person': None,
+        'progress_report': progress_report,
+        'programme_document': pd,
+    }
+
+    to_emails = set()
+    to_emails |= set(map(lambda person: person, pd.unicef_officers.all()))
+    to_emails |= set(map(lambda person: person, pd.unicef_focal_point.all()))
+
+    for person in to_emails:
+        send_email_from_template(
+            subject_template_path='emails/due_progress_report_subject.txt',
+            body_template_path='emails/due_progress_report.html',
+            template_data=template_data,
+            to_email_list=[person.email, ],
+            content_subtype='html'
+        )
+
+
+def send_overdue_progress_report_email(progress_report):
+    """send_overdue_progress_report_email sends email notifications to
+    UNICEF Authorized Officers and Focal Points about overdue reports.
+
+    Arguments:
+        progress_report {ProgressReport} -- ProgressReport ORM object to send email notification about
+    """
+
+    pd = progress_report.programme_document
+
+    template_data = {
+        'person': None,
+        'progress_report': progress_report,
+        'programme_document': pd,
+    }
+
+    to_emails = set()
+    to_emails |= set(map(lambda person: person, pd.unicef_officers.all()))
+    to_emails |= set(map(lambda person: person, pd.unicef_focal_point.all()))
+
+    for person in to_emails:
+        send_email_from_template(
+            subject_template_path='emails/overdue_progress_report_subject.txt',
+            body_template_path='emails/overdue_progress_report.html',
+            template_data=template_data,
+            to_email_list=[person.email, ],
+            content_subtype='html'
+        )
