6486277856b0e2f5fcd903179087a737d80f9b57
Silin Na <n1207n@gmail.com>
2019-03-13 11:34:19 -0700

    feat: Created 2 python utility functions to delete IndicatorReports and IndicatorLocationData instances [#1094]
    + delete_all_ilds_for_ir
    
    + delete_all_irs_for_pr

--- django_api/django_api/apps/indicator/utilities.py
+++ django_api/django_api/apps/indicator/utilities.py
@@ -14,3 +14,25 @@ def format_total_value_to_string(total, is_percentage=False):
 
     locale = to_locale(get_language())
     return formatter(value, locale=locale)
+
+
+def delete_all_ilds_for_ir(indicator_report):
+    """Delete all IndicatorLocationData instances for given IndicatorReport instance.
+
+    Arguments:
+        indicator_report {IndicatorReport} -- IndicatorReport instance to delete its location data from
+    """
+
+    indicator_report.indicator_location_data.all().delete()
+
+
+def delete_all_irs_for_pr(progress_report):
+    """Delete all IndicatorReport instances for given ProgressReport instance.
+
+    Arguments:
+        progress_report {ProgressReport} -- ProgressReport instance to delete its indicator reports from
+    """
+
+    for ir in progress_report.indicator_reports.all():
+        delete_all_ilds_for_ir(ir)
+        ir.delete()
