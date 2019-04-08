e3ca65286ecdd1aa0bcc73f1d651b642e78b29a8
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-08 15:26:20 -0700

    feat: Verify web-component-tester works locally and tweak imports [ch10878]

--- polymer/test/unit/test.js
+++
@@ -1,7 +0,0 @@
-// var basic = require('../../src/elements/etools-prp-ajax.html');
-
-describe('frontend tests', function() {
-  it('should pass', function() {
-    expect(true).toBe(true);
-  });
-});
