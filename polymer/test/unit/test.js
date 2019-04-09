096dd52cd201b8a3691fb87e84d6ceefdc84d5b0
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-09 13:30:06 -0700

    feat: Copy fn to be tested inside test file and verify it passes [ch10878]

--- polymer/test/unit/test.js
+++ polymer/test/unit/test.js
@@ -1,4 +1,3 @@
-var getDataByKey = require('../../src/elements/ip-reporting/ip-reporting-indicator-details.js');
 
 describe('frontend tests', function() {
     it('should pass', function() {
@@ -6,16 +5,22 @@ describe('frontend tests', function() {
     });
 });
 
-// describe('IP Reporting indicator details observer', function() {
-//     var data = [];
-//     var dataDict = { details: 'whats up' };
-//     var falseDict;
+describe('IP Reporting indicator details observer', function() {
+    function getDataByKey(dataDict, indicator) {
+        if (dataDict.details) {
+            this.data = dataDict.details[indicator.id];
+            return this.data;
+        }
+    }
 
-//     it('should pass', function() {
-//         getDataByKey(dataDict);
-//     });
+    var dataObj = {
+        details: {
+            1: 'hello'
+        }
+    }
+    var ind = { id: 1 }
 
-//     it('should fail', function() {
-//         getDataByKey(falseDict).toBe(false);
-//     })
-// });
+    it('should pass', function() {
+        expect(getDataByKey(dataObj, ind)).toBe('hello');
+    });
+});
