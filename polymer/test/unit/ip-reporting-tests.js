de27fdda84cac9d5e21ec51e9d7ff82f1c86ac5e
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-09 15:30:17 -0700

    feat: Add test cases for computeParams function [ch10878]

--- polymer/test/unit/ip-reporting-tests.js
+++ polymer/test/unit/ip-reporting-tests.js
@@ -30,12 +30,14 @@ describe('IP reporting indicator details observer', function() {
     it('should equal hello', function() {
         expect(getDataByKey(dataObj, ind)).toBe('hello');
     });
+
     it('should return undefined if dataDict has no details', function() {
         expect(getDataByKey(badObj, ind)).toBe(undefined);
     });
+
     it('should return undefined if dataDict is not an object', function() {
         expect(getDataByKey(nullObj, ind)).toBe(undefined);
-    })
+    });
 });
 
 describe('IP reporting indicator details clusterApp function', function() {
@@ -49,10 +51,38 @@ describe('IP reporting indicator details clusterApp function', function() {
     it('should return true if name is cluster-reporting', function() {
         expect(computeIsClusterApp(appName)).toBe(true);
     });
+
     it('should return false if name is not cluster-reporting', function() {
         expect(computeIsClusterApp(ipName)).toBe(false);
     });
+
     it('should return false if invalid name or empty string is given', function() {
         expect(computeIsClusterApp('')).toBe(false);
+    });
+});
+
+describe('IP reporting indicator details computeParams function', function() {
+    function computeParams(boolean) {
+        var params = {};
+    
+        if (boolean === true) {
+            params = Object.assign({}, params, {
+                hide_children: 1,
+            });
+        }
+    
+        return params;
+    }
+
+    var isClusterApp = true;
+    var isNotClusterApp = false;
+    var trueParams = { hide_children: 1 };
+
+    it('should return empty params object if boolean is false', function() {
+        expect(computeParams(isNotClusterApp)).toEqual({});
+    });
+
+    it('should return object with hide_children property if boolean is true', function() {
+        expect(computeParams(isClusterApp)).toEqual(trueParams);
     });
 });
