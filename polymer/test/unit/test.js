a8005ff284c842a5733cdb217702e7fc6300ac1d
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-09 13:48:24 -0700

    feat: Add additional tests for fail cases for getDataByKey fn [ch10878]

--- polymer/test/unit/test.js
+++ polymer/test/unit/test.js
@@ -9,7 +9,7 @@ describe('IP Reporting indicator details observer', function() {
     function getDataByKey(dataDict, indicator) {
         if (dataDict.details) {
             this.data = dataDict.details[indicator.id];
-            return this.data;
+            return this.data;  // Not in original function! For testing purposes only
         }
     }
 
@@ -18,9 +18,21 @@ describe('IP Reporting indicator details observer', function() {
             1: 'hello'
         }
     }
-    var ind = { id: 1 }
+    var ind = {
+        id: 1
+    }
+    var badObj = {
+        deets: 'howdy'
+    }
+    var nullObj = '';
 
     it('should pass', function() {
         expect(getDataByKey(dataObj, ind)).toBe('hello');
     });
+    it('should not pass if dataDict has no details', function() {
+        expect(getDataByKey(badObj, ind)).toBe(undefined);
+    });
+    it('should not pass if dataDict is not an object', function() {
+        expect(getDataByKey(nullObj, ind)).toBe(undefined);
+    })
 });
