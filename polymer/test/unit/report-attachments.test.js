ff55ee59e65197dbdd60404cda9805259fc82c1c
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-11 12:38:41 -0700

    feat: Finish writing test case for computeListUrl [ch10878]

--- polymer/test/unit/report-attachments.test.js
+++ polymer/test/unit/report-attachments.test.js
@@ -1,12 +1,28 @@
 const ReportAttachmentsUtils = require('../../src/elements/ip-reporting/js/report-attachments.js');
 
-const { computeListUrl } = ReportAttachmentsUtils;
-
 describe('Report attachments computeListUrl function', () => {
+    // found in polymer/src/endpoints.html
+    const _buildUrl = tail => '/api' + tail;
+
+    // found in polymer/src/endpoints.html
+    const progressReports = locationId => {
+        return _buildUrl('/unicef/' + locationId + '/progress-reports/');
+    }
+
+    // found in polymer/src/endpoints.html
+    const progressReportAttachments = (locationId, reportId) => {
+        return progressReports(locationId) + reportId + '/attachments/';
+    }
+
+    // found in polymer/src/elements/ip-reporting/report-attachments.js
+    const computeListUrl = (locationId, reportId) => {
+        return progressReportAttachments(locationId, reportId);
+    }
+
     const reportId = 1;
     const locationId = 2;
 
-    it('works?', () => {
+    it('builds the URL correctly', () => {
         expect(computeListUrl(locationId, reportId)).toBe('/api/unicef/2/progress-reports/1/attachments/')
     });
 });
