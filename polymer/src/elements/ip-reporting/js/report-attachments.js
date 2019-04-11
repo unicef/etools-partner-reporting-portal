40816bf5c9ea602289c59b00df73f58cc22a85e6
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-11 14:29:17 -0700

    feat: Extract getDeleteUrl logic from report-attachments component and write test cases [ch10878]

--- polymer/src/elements/ip-reporting/js/report-attachments.js
+++ polymer/src/elements/ip-reporting/js/report-attachments.js
@@ -6,6 +6,9 @@ ReportAttachmentsUtils.computeListUrl = function(locationId, reportId) {
     return App.Endpoints.progressReportAttachments(locationId, reportId);
 }
 
+ReportAttachmentsUtils.getDeleteUrl = function(locationId, reportId, attachmentId) {
+    return App.Endpoints.progressReportAttachmentDetail(locationId, reportId, attachmentId);
+}
 
 try {
     module.exports = exports = ReportAttachmentsUtils;
--- polymer/test/unit/report-attachments.test.js
+++ polymer/test/unit/report-attachments.test.js
@@ -1,14 +1,15 @@
 const ReportAttachmentsUtils = require('../../src/elements/ip-reporting/js/report-attachments.js');
 
+// Functions for testing endpoints - TODO: FIX THIS so it's actually importing
+// found in polymer/src/endpoints.html
+const _buildUrl = tail => '/api' + tail;
+
+// found in polymer/src/endpoints.html
+const progressReports = locationId => {
+    return _buildUrl('/unicef/' + locationId + '/progress-reports/');
+}
+
 describe('Report attachments computeListUrl function', () => {
-    // found in polymer/src/endpoints.html
-    const _buildUrl = tail => '/api' + tail;
-
-    // found in polymer/src/endpoints.html
-    const progressReports = locationId => {
-        return _buildUrl('/unicef/' + locationId + '/progress-reports/');
-    }
-
     // found in polymer/src/endpoints.html
     const progressReportAttachments = (locationId, reportId) => {
         return progressReports(locationId) + reportId + '/attachments/';
@@ -19,10 +20,31 @@ describe('Report attachments computeListUrl function', () => {
         return progressReportAttachments(locationId, reportId);
     }
 
-    const reportId = 1;
-    const locationId = 2;
+    const locationId = 1;
+    const reportId = 2;
 
-    it('builds the URL correctly', () => {
-        expect(computeListUrl(locationId, reportId)).toBe('/api/unicef/2/progress-reports/1/attachments/')
+    it('builds the attachment URL correctly', () => {
+        expect(computeListUrl(locationId, reportId)).toBe('/api/unicef/1/progress-reports/2/attachments/')
+    });
+});
+
+describe('Report attachments getDeleteUrl function', () => {
+    // found in polymer/src/endpoints.html
+    const progressReportAttachmentDetail = (locationId, reportId, attachmentId) => {
+        return progressReports(locationId) + reportId + '/attachments/' + attachmentId + '/';
+    }
+    
+    // found in polymer/src/elements/ip-reporting/report-attachments.js
+    const getDeleteUrl = (locationId, reportId, attachmentId) => {
+        return progressReportAttachmentDetail(locationId, reportId, attachmentId);
+    }
+
+    const locationId = 1;
+    const reportId = 2;
+    const attachmentId = 3;
+
+
+    it('builds the delete URL correctly', () => {
+        expect(getDeleteUrl(locationId, reportId, attachmentId)).toBe('/api/unicef/1/progress-reports/2/attachments/3/')
     });
 });
