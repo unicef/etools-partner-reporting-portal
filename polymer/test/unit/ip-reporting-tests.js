9198b96afaced5b914b5b73659dad8f786201af5
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-11 10:50:05 -0700

    style: Refactor Indicator Details tests to use ES6 syntax instead of ES5 [ch10878]

--- polymer/test/unit/ip-reporting-tests.js
+++ polymer/test/unit/ip-reporting-tests.js
@@ -1,125 +1,121 @@
-var IndicatorDetailsUtils = require('../../src/elements/ip-reporting/js/ip-reporting-indicator-details.js');
+const IndicatorDetailsUtils = require('../../src/elements/ip-reporting/js/ip-reporting-indicator-details.js');
 
-var { getDataByKey,
+const { getDataByKey,
     computeIsClusterApp,
     computeParams,
     computeHidden,
     bucketByLocation } = IndicatorDetailsUtils;
 
 
-describe('frontend tests', function() {
-    it('should pass', function() {
+describe('frontend tests', () => {
+    it('should pass', () => {
         expect(true).toBe(true);
     });
 });
 
-describe('IP reporting indicator details observer', function() {
+describe('IP reporting indicator details observer', () => {
     // found in polymer/src/elements/ip-reporting/ip-reporting-indicator-details.js
-    var dataObj = {
+    const dataObj = {
         details: {
             1: 'hello'
         }
     }
 
-    var ind = { id: 1 }
-    var badObj = { deets: 'howdy' }
-    var nullObj = '';
+    const ind = { id: 1 }
+    const badObj = { deets: 'howdy' }
+    const nullObj = '';
 
-    xit('should equal hello', function() {
+    xit('should equal hello', () => {
         expect(getDataByKey(dataObj, ind)).toBe('hello');
     });
 
-    it('should return undefined if dataDict has no details', function() {
+    it('should return undefined if dataDict has no details', () => {
         expect(getDataByKey(badObj, ind)).toBe(undefined);
     });
 
-    it('should return undefined if dataDict is not an object', function() {
+    it('should return undefined if dataDict is not an object', () => {
         expect(getDataByKey(nullObj, ind)).toBe(undefined);
     });
 });
 
-describe('IP reporting indicator details clusterApp function', function() {
+describe('IP reporting indicator details clusterApp function', () => {
     // found in polymer/src/elements/ip-reporting/ip-reporting-indicator-details.js
-    var appName = 'cluster-reporting';
-    var ipName = 'ip-reporting';
+    const appName = 'cluster-reporting';
+    const ipName = 'ip-reporting';
 
-    it('should return true if name is cluster-reporting', function() {
+    it('should return true if name is cluster-reporting', () => {
         expect(computeIsClusterApp(appName)).toBe(true);
     });
 
-    it('should return false if name is not cluster-reporting', function() {
+    it('should return false if name is not cluster-reporting', () => {
         expect(computeIsClusterApp(ipName)).toBe(false);
     });
 
-    it('should return false if invalid name or empty string is given', function() {
+    it('should return false if invalid name or empty string is given', () => {
         expect(computeIsClusterApp('')).toBe(false);
     });
 });
 
-describe('IP reporting indicator details computeParams function', function() {
+describe('IP reporting indicator details computeParams function', () => {
     // found in polymer/src/elements/ip-reporting/ip-reporting-indicator-details.js
-    var isClusterApp = true;
-    var isNotClusterApp = false;
-    var trueParams = { hide_children: 1 };
+    const isClusterApp = true;
+    const isNotClusterApp = false;
+    const trueParams = { hide_children: 1 };
 
-    it('should return empty params object if boolean is false', function() {
+    it('should return empty params object if boolean is false', () => {
         expect(computeParams(isNotClusterApp)).toEqual({});
     });
 
-    it('should return object with hide_children property if boolean is true', function() {
+    it('should return object with hide_children property if boolean is true', () => {
         expect(computeParams(isClusterApp)).toEqual(trueParams);
     });
 });
 
-describe('IP reporting indicator details computeIndicatorReportsUrl function', function() {
+describe('IP reporting indicator details computeIndicatorReportsUrl function', () => {
     // found in polymer/src/endpoints.html
-    function _buildUrl(tail) {
-        return '/api' + tail;
-    }
+    const _buildUrl = tail => '/api' + tail;
 
     // found in polymer/src/endpoints.html
-    function indicatorReports(reportableId) {
-        return _buildUrl('/indicator/' + reportableId + '/indicator-reports/');
-    }
+    const indicatorReports = reportableId => _buildUrl('/indicator/' + reportableId + '/indicator-reports/');
 
     // found in polymer/src/elements/ip-reporting/ip-reporting-indicator-details.js
-    function computeIndicatorReportsUrl(indicator) {
-        var target_indicator_id = indicator.cluster_partner_indicator_reportable_id
+    const computeIndicatorReportsUrl = indicator => {
+        const target_indicator_id = indicator.cluster_partner_indicator_reportable_id
             ? indicator.cluster_partner_indicator_reportable_id
             : indicator.id;
         return indicatorReports(target_indicator_id) + '?limit=2';
     }
 
-    var indicator = { id: 1489 };
-    var clusterIndicator = { cluster_partner_indicator_reportable_id: 1489, id: 1490 };
+    const indicator = { id: 1489 };
+    const clusterIndicator = { cluster_partner_indicator_reportable_id: 1489, id: 1490 };
 
-    it('should return correct URL with id', function() {
+    it('should return correct URL with id', () => {
         expect(computeIndicatorReportsUrl(indicator)).toBe('/api/indicator/1489/indicator-reports/?limit=2');
     });
 
-    it('should use cluster_partner_indicator_reportable_id rather than just id if it exists', function() {
+    it('should use cluster_partner_indicator_reportable_id rather than just id if it exists', () => {
         expect(computeIndicatorReportsUrl(clusterIndicator)).toBe('/api/indicator/1489/indicator-reports/?limit=2')
         expect(computeIndicatorReportsUrl(clusterIndicator)).not.toBe('/api/indicator/1490/indicator-reports/?limit=2')
     });
 });
 
-describe('IP reporting indicator details computeHidden function', function() {
-    var data = ['test', 'test2'];
-    var loading = undefined;
-    var definedLoading = true;
+describe('IP reporting indicator details computeHidden function', () => {
+    const data = ['test', 'test2'];
+    const loading = undefined;
+    const definedLoading = true;
 
-    it('should return true', function() {
+    it('should return true', () => {
         expect(computeHidden(data, loading)).toBe(true);
     });
 
-    it('should return length of data array when loading is true', function() {
+    it('should return length of data array when loading is true', () => {
         expect(computeHidden(data, definedLoading)).toBe(2);
     });
 });
 
-describe('IP reporting indicator details bucketByLocation function', function() {
-    var data;
-    var bigData = [{
+describe('IP reporting indicator details bucketByLocation function', () => {
+    let data;
+    const bigData = [{
         id: 0,
         indicator_location_data: [{ 
             location: { 
@@ -138,7 +134,7 @@ describe('IP reporting indicator details bucketByLocation function', function() 
         }]
     }];
 
-    var greatExpectations = [{
+    const greatExpectations = [{
         reportInfo: {
             previous: {
                 id: 1,
@@ -179,11 +175,11 @@ describe('IP reporting indicator details bucketByLocation function', function() 
         name: 'title1'
     }];
 
-    it('should return an empty array', function() {
+    it('should return an empty array', () => {
         expect(bucketByLocation(data)).toEqual([]);
     });
 
-    it('should return locationList array when given bigData', function() {
+    it('should return locationList array when given bigData', () => {
         expect(bucketByLocation(bigData)).toEqual(greatExpectations);
     });
 });
