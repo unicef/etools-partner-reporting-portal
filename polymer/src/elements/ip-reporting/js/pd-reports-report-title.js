5a53f21236668cf467bc3abfe0cca182fc2a2be8
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-12 16:09:37 -0700

    feat: Extract getReportTitleFull logic from pd-reports-report-title component and add unit tests [ch10878]

--- polymer/src/elements/ip-reporting/js/pd-reports-report-title.js
+++ polymer/src/elements/ip-reporting/js/pd-reports-report-title.js
@@ -6,6 +6,18 @@ PdReportsReportTitleUtils.shouldDisplayLink = function (displayLink, report, per
     return displayLink && fn(permissions, report);
 }
 
+PdReportsReportTitleUtils.getReportTitleFull = function (report) {
+    var title =  report.report_type + report.report_number;
+    if (report.report_type === 'QPR') {
+        title += ' (Quarterly Progress Report)';
+    } else if (report.report_type === 'HR') {
+        title += ' (Humanitarian Report)';
+    } else if (report.report_type === 'SR') {
+        title += ' (Special Report)';
+    }
+    return title;
+}
+
 try {
     module.exports = exports = PdReportsReportTitleUtils;
 } catch (e) {}
--- polymer/test/unit/pd-reports-report-title.test.js
+++ polymer/test/unit/pd-reports-report-title.test.js
@@ -1,8 +1,8 @@
 const PdReportsReportTitleUtils = require('../../src/elements/ip-reporting/js/pd-reports-report-title.js');
 
-const { shouldDisplayLink } = PdReportsReportTitleUtils;
+const { shouldDisplayLink, getReportTitleFull } = PdReportsReportTitleUtils;
 
-describe('Pd Report Report Title shouldDisplayLink function', () => {
+describe('Pd Reports Report Title shouldDisplayLink function', () => {
     const link = true;
     const report = { id: 111 };
     const permissions = { editPermissions: true };
@@ -10,5 +10,20 @@ describe('Pd Report Report Title shouldDisplayLink function', () => {
 
     it('returns true when provided with truthy values', () => {
         expect(shouldDisplayLink(link, report, permissions, fn)).toBe(true);
+    });
+});
+
+describe('Pd Reports Report Title getReportTitleFull function', () => {
+    const reportQpr = { report_type: 'QPR', report_number: 1138 };
+    const reportHr = { report_type: 'HR', report_number: 667 };
+    const reportSr = { report_type: 'SR', report_number: 451 };
+
+    it('returns the correct full report title', () => {
+        expect(getReportTitleFull(reportQpr)).toBe('QPR1138 (Quarterly Progress Report)');
+    });
+
+    it('returns the correct report title for all types', () => {
+        expect(getReportTitleFull(reportHr)).toBe('HR667 (Humanitarian Report)');
+        expect(getReportTitleFull(reportSr)).toBe('SR451 (Special Report)');
     });
 });
