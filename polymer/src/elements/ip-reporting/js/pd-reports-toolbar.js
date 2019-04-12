01a92318e6ce5222271f37c1a01bf94a45ffe892
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-12 14:34:58 -0700

    feat: Finish extracting other function logic from pd-reports-toolbar component and add unit tests for each [ch10878]

--- polymer/src/elements/ip-reporting/js/pd-reports-toolbar.js
+++ polymer/src/elements/ip-reporting/js/pd-reports-toolbar.js
@@ -6,6 +6,16 @@ PdReportsToolbarUtils.computePdReportsUrl = function (locationId) {
     return App.Endpoints.programmeDocumentReports(locationId);
 }
 
+PdReportsToolbarUtils.canExport = function (totalResults) {
+    return totalResults > 0;
+}
+
+PdReportsToolbarUtils.computePdQuery = function (pdId) {
+    return {
+        programme_document: pdId,
+    };
+}
+
 try {
     module.exports = exports = PdReportsToolbarUtils;
 } catch (e) {}
--- polymer/test/unit/pd-reports-toolbar.test.js
+++ polymer/test/unit/pd-reports-toolbar.test.js
@@ -1,5 +1,7 @@
 const PdReportsToolbarUtils = require('../../src/elements/ip-reporting/js/pd-reports-toolbar.js');
 
+const { canExport, computePdQuery } = PdReportsToolbarUtils;
+
 describe('Pd Reports Toolbar computePdReportsUrl function', () => {
     // found in polymer/src/endpoints.html
     const _buildUrl = tail => {
@@ -20,5 +22,26 @@ describe('Pd Reports Toolbar computePdReportsUrl function', () => {
 
     it('returns correct url', () => {
         expect(computePdReportsUrl(locationId)).toBe('/api/unicef/10/progress-reports/');
+    });
+});
+
+describe('Pd Reports Toolbar canExport function', () => {
+    const totalResults = 5;
+    const zeroResults = 0;
+
+    it('returns true if number is greater than 0', () => {
+        expect(canExport(totalResults)).toBe(true);
+    });
+
+    it('returns false if number is 0 or less than 0', () => {
+        expect(canExport(zeroResults)).toBe(false);
+    });
+});
+
+describe('Pd Reports Toolbar computePdQuery function', () => {
+    const id = '800';
+
+    it('returns an object with the correct id', () => {
+        expect(computePdQuery(id)).toEqual({ programme_document: '800' });
     });
 });
