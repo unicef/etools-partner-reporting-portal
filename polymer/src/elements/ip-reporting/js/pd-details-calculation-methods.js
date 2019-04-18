b404ee4b3f7a20680cc99bcb21952602023e71cf
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-18 15:08:44 -0700

    feat: Extract computeFormattedData from PdDetailsCalculationMethods and add unit test [ch10878]

--- polymer/src/elements/ip-reporting/js/pd-details-calculation-methods.js
+++ polymer/src/elements/ip-reporting/js/pd-details-calculation-methods.js
@@ -6,6 +6,31 @@ PdDetailsCalculationMethodsUtils.computeIndicatorsUrl = function(locationId, pdI
     return locationId ? App.Endpoints.calculationMethods(locationId, pdId) : '';
 };
 
+PdDetailsCalculationMethodsUtils.computeFormattedData = function(data) {
+    return data.ll_outputs_and_indicators.reduce(function (acc, curr) {
+        var items;
+
+        acc.push({
+            type: 'label',
+            text: curr.ll_output.title,
+        });
+
+        items = curr.indicators.map(function (indicator) {
+            return {
+                type: 'data',
+                data: Object.assign({}, indicator, {
+                    editable: true,
+                }),
+                llo_id: curr.ll_output.id,
+            };
+        });
+
+        acc.push.apply(acc, items);
+
+        return acc;
+    }, []);
+};
+
 try {
     module.exports = exports = PdDetailsCalculationMethodsUtils;
 } catch (e) {}
--- polymer/test/unit/pd-details-calculation-methods.test.js
+++ polymer/test/unit/pd-details-calculation-methods.test.js
@@ -1,3 +1,6 @@
+const PdDetailsCalculationMethodsUtils = require('../../src/elements/ip-reporting/js/pd-details-calculation-methods');
+const {computeFormattedData} = PdDetailsCalculationMethodsUtils;
+
 // These functions are from endpoints.html
 const _buildUrl = tail => '/api' + tail;
 
@@ -19,6 +22,26 @@ describe('PdDetailsCalculationMethods functions', () => {
         it('builds the url correctly', () => {
             expect(calculationMethods(locationId, pdId))
                 .toBe('/api/unicef/451/programme-document/667/calculation-methods/');
+        });
+    });
+
+    describe('computeFormattedData function', () => {
+        const data = {
+            ll_outputs_and_indicators: [
+                {
+                    ll_output: {id: 5, title: 'hello'},
+                    indicators: [{a: 1}]
+                }
+            ]
+        };
+
+        const formatted = [
+            {type: 'label', text: 'hello'},
+            {type: 'data', data: {a: 1, editable: true}, llo_id: 5}
+        ];
+
+        it('builds the correct array from a given object', () => {
+            expect(computeFormattedData(data)).toEqual(formatted);
         });
     });
 });
