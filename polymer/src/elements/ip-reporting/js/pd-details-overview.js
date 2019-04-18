a8db99c917c348713dd24db7f6788bf6a9340ec8
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-18 11:54:21 -0700

    feat: Extract hasAmendments logic from PdDetailsOverview and add unit tests [ch10878]

--- polymer/src/elements/ip-reporting/js/pd-details-overview.js
+++ polymer/src/elements/ip-reporting/js/pd-details-overview.js
@@ -6,6 +6,10 @@ PdDetailsOverviewUtils.computeLoaded = function(pd) {
     return !!pd.id;
 };
 
+PdDetailsOverviewUtils.hasAmendments = function(pd) {
+    return pd.amendments && !!pd.amendments.length;
+};
+
 try {
     module.exports = exports = PdDetailsOverviewUtils;
 } catch (e) {}
--- polymer/test/unit/pd-details-overview.test.js
+++ polymer/test/unit/pd-details-overview.test.js
@@ -1,15 +1,32 @@
 const PdDetailsOverviewUtils = require('../../src/elements/ip-reporting/js/pd-details-overview');
-const {computeLoaded} = PdDetailsOverviewUtils;
+const {
+    computeLoaded,
+    hasAmendments} = PdDetailsOverviewUtils;
 
-describe('PdDetailsOverview computedLoaded function', () => {
-    const pd = {id: 491};
-    const otherPd = {id: ''};
-
-    it('returns true when pd object has truthy id value', () => {
-        expect(computeLoaded(pd)).toBe(true);
+describe('PdDetailsOverview functions', () => {
+    describe('computedLoaded function', () => {
+        const pd = {id: 491};
+        const otherPd = {id: ''};
+    
+        it('returns true when pd object has truthy id value', () => {
+            expect(computeLoaded(pd)).toBe(true);
+        });
+    
+        it('returns false when pd object has falsy id value', () => {
+            expect(computeLoaded(otherPd)).toBe(false);
+        });
     });
+    
+    describe('hasAmendments function', () => {
+        const pd = {'amendments': [667, 451]};
+        const otherPd = {'amendments': []};
+    
+        it('returns true when pd object has amendments array property with items in it', () => {
+            expect(hasAmendments(pd)).toBe(true);
+        });
 
-    it('returns false when pd object has falsy id value', () => {
-        expect(computeLoaded(otherPd)).toBe(false);
+        it('returns false when pd object has empty amendments array', () => {
+            expect(hasAmendments(otherPd)).toBe(false);
+        });
     });
 });
