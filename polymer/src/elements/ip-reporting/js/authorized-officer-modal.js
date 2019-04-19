7c2ee95c7e9a4b6c8a4f703437abe36094532201
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-19 12:07:37 -0700

    feat: Extract computeAuthorizedPartners from AuthorizedOfficerModal and add unit test [ch10878]

--- polymer/src/elements/ip-reporting/js/authorized-officer-modal.js
+++ polymer/src/elements/ip-reporting/js/authorized-officer-modal.js
@@ -8,6 +8,19 @@ AuthorizedOfficerModalUtils.computePostBody = function(selectedFocalPoint) {
     };
 };
 
+AuthorizedOfficerModalUtils.computeAuthorizedPartners = function(pd) {
+    var partners = (pd.unicef_officers || []).filter(function(partner) {
+        return partner.is_authorized_officer && partner.active;
+    });
+    var formattedPartners = partners.map(function (partner) {
+        return {
+            value: partner.email,
+            title: partner.name + ' ' + partner.title
+        };
+    });
+    return formattedPartners;
+};
+
 try {
     module.exports = exports = AuthorizedOfficerModalUtils;
 } catch (e) {}
--- polymer/test/unit/authorized-officer-modal.test.js
+++ polymer/test/unit/authorized-officer-modal.test.js
@@ -1,5 +1,8 @@
 const AuthorizedOfficerModalUtils = require('../../src/elements/ip-reporting/js/authorized-officer-modal');
-const {computePostBody} = AuthorizedOfficerModalUtils;
+const {
+    computePostBody,
+    computeAuthorizedPartners
+    } = AuthorizedOfficerModalUtils;
 
 describe('AuthorizedOfficerModal functions', () => {
     describe('computePostBody function', () => {
@@ -7,6 +10,35 @@ describe('AuthorizedOfficerModal functions', () => {
 
         it('builds the object', () => {
             expect(computePostBody(selectedFocalPoint)).toEqual({submitted_by_email: 'test@test.org'});
+        });
+    });
+
+    describe('computeAuthorizedPartners function', () => {
+        const pd = {
+            unicef_officers: [
+                {
+                    is_authorized_officer: true,
+                    active: true,
+                    email: 'pseudo@nym.com',
+                    name: 'George Eliot',
+                    title: 'Author'
+                },
+                {
+                    is_authorized_officer: false,
+                    active: true,
+                    email: 'real@name.com',
+                    name: 'Mary Ann Evans',
+                    title: 'Writer'
+                }
+            ]
+        };
+
+        const formatted = [
+            {value: 'pseudo@nym.com', title: 'George Eliot Author'}
+        ];
+
+        it('returns the correctly-formatted authorized partners', () => {
+            expect(computeAuthorizedPartners(pd)).toEqual(formatted);
         });
     });
 });
