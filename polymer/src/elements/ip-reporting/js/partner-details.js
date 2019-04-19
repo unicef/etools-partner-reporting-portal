b68e92780daa8ec140b2d9e6defe3f68e7329e37
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-19 09:55:45 -0700

    feat: Extract all logic from PartnerDetails component and add unit test [ch10878]

---
+++ polymer/src/elements/ip-reporting/js/partner-details.js
@@ -0,0 +1,16 @@
+function PartnerDetailsUtils() {
+
+};
+
+PartnerDetailsUtils.computePartnerType = function(partner, withDefaultFn) {
+    var chunks = [
+        partner.cso_type_display,
+        partner.partner_type_display,
+    ].filter(Boolean);
+
+    return withDefaultFn(chunks.length ? chunks.join(' ') : null);
+};
+
+try {
+    module.exports = exports = PartnerDetailsUtils;
+} catch (e) {}
