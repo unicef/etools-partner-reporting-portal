19c3b73a860aa302e612a715ccdf0c0ec78e520e
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-23 11:21:42 -0700

    fix: Add button click to ButtonClear snapshot test [ch11331]

--- react/id_management_frontend/src/components/common/test/ButtonClear.test.js
+++ react/id_management_frontend/src/components/common/test/ButtonClear.test.js
@@ -4,13 +4,17 @@ import toJSON from 'enzyme-to-json';
 import ButtonClear from '../ButtonClear';
 
 describe('Clear button component', () => {
-    it('renders clear button', () => {
+    it('renders clear button and calls click', () => {
         const onClick = jest.fn();
 
         const wrapper = shallow(<ButtonClear
             onClick={onClick}
         />);
 
+        wrapper.simulate('click');
+        const calls = onClick.mock.calls;
+
+        expect(calls.length).toBe(1);
         expect(toJSON(wrapper)).toMatchSnapshot();
     });
 });
