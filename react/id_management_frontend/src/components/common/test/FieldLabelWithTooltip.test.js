9b010c179665e9d5724470314057e8296032a7a2
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-04-25 10:34:24 -0700

    feat: Add new test to verify conditional rendering for full line coverage [ch11331]

--- react/id_management_frontend/src/components/common/test/FieldLabelWithTooltip.test.js
+++ react/id_management_frontend/src/components/common/test/FieldLabelWithTooltip.test.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import toJSON from 'enzyme-to-json';
 import FieldLabelWithTooltip from '../FieldLabelWithTooltip';
 import TooltipIcon from '../TooltipIcon';
 
 describe('FieldLabelWithTooltip component', () => {
+    const children = <div></div>
+    const classes = {};
+    const tooltipIconProps = {};
+    const labelProps = {};
+    
     it('renders properly', () => {
-        const children = <div></div>
-        const classes = {};
         const infoText = 'help';
-        const tooltipIconProps = {};
-        const labelProps = {};
 
         const wrapper = shallow(<FieldLabelWithTooltip
             children={children}
@@ -25,20 +26,16 @@ describe('FieldLabelWithTooltip component', () => {
     });
 
     it('renders null if infoText is falsy', () => {
-        const newInfoText = '';
-        const children = <div></div>
-        const classes = {};
-        const tooltipIconProps = {};
-        const labelProps = {};
+        const infoText = '';
 
-        const wrapper = shallow(<FieldLabelWithTooltip
+        const wrapper = mount(<FieldLabelWithTooltip
             children={children}
             classes={classes}
-            infoText={newInfoText}
+            infoText={infoText}
             tooltipIconProps={tooltipIconProps}
             labelProps={labelProps}
         />);
 
-        expect(wrapper.contains(<TooltipIcon/>)).toBe(false);
+        expect(wrapper.exists(TooltipIcon)).toBe(false);
     });
 });
