e9c1fe3d0f1a52f748481fa334b4f75b019e9680
Injoong Yoon <27440940+AttilaTheHen@users.noreply.github.com>
2019-05-13 17:57:53 -0700

    feat: Add snapshot test for onSubmit async method and begin to test [ch11331]

--- react/id_management_frontend/src/components/Users/test/AddPermissionsDialog.test.js
+++ react/id_management_frontend/src/components/Users/test/AddPermissionsDialog.test.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import toJSON from 'enzyme-to-json';
+import 'babel-polyfill';
 import {AddPermissionsDialog} from '../AddPermissionsDialog';
+
+jest.mock('../../../infrastructure/api', () => ({
+    api: {
+        post: jest.fn(() => Promise.resolve({}))
+    }
+}))
 
 describe('AddPermissionsDialog component', () => {
     const error = 'this is an error';
@@ -12,6 +19,7 @@ describe('AddPermissionsDialog component', () => {
     const reset = jest.fn();
     const user = {};
     const width = 'md';
+    const id = {id: 1138};
 
     const wrapper = shallow(<AddPermissionsDialog
         error={error}
@@ -22,6 +30,7 @@ describe('AddPermissionsDialog component', () => {
         reset={reset}
         user={user}
         width={width}
+        id={id}
     />);
 
     it('renders the component correctly', () => {
@@ -37,5 +46,12 @@ describe('AddPermissionsDialog component', () => {
 
         expect(closeCalls.length).toBe(1);
         expect(resetCalls.length).toBe(1);
+    });
+
+    it('calls onSubmit method correctly', () => {
+        wrapper.instance().onSubmit({user_id: null})
+            .then(res => {
+                console.log(res);
+            })
     });
 });
