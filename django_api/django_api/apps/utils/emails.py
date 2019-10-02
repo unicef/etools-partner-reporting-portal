170ec5d36df9c37d6e7cef997b8dc8fc0acb4d51
Silin Na <n1207n@gmail.com>
2019-10-02 12:44:20 -0700

    lint: Removed unused imports

--- django_api/django_api/apps/utils/emails.py
+++ django_api/django_api/apps/utils/emails.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.template.loader import get_template
 from post_office.models import EmailTemplate
 from unicef_notification.models import Notification
-from unicef_notification.utils import send_notification_with_template
 
 
 logger = logging.getLogger(__name__)
