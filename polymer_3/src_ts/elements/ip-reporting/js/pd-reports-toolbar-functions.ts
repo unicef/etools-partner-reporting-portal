7ce7a84363e9f68eb01ae12ab1a95d7a9d3ceedf
dcioara <dan.cioara@nordlogic.com>
2020-02-19 13:49:02 +0200

    [ch18350] Migrate ip-reporting/js folder to Polymer3

---
+++ polymer_3/src_ts/elements/ip-reporting/js/pd-reports-toolbar-functions.ts
@@ -0,0 +1,15 @@
+import Endpoints from '../../../endpoints';
+
+export function computePdReportsUrl(locationId: string) {
+  return Endpoints.programmeDocumentReports(locationId);
+}
+
+export function canExport(totalResults: number) {
+  return totalResults > 0;
+}
+
+export function computePdQuery(pdId: string) {
+  return {
+    programme_document: pdId,
+  };
+}
