6327fb782b5a06321374232905251f9400e49956
lajoskovago <lajos.kovago91@gmail.com>
2020-02-19 11:30:28 +0200

    migarted disaggregation field and modal

---
+++ polymer_3/src_ts/elements/disaggregations/disaggregation-field.ts
@@ -0,0 +1,107 @@
+import {html} from '@polymer/polymer';
+import '@polymer/paper-input/paper-input';
+import DisaggregationMixin from '../../mixins/disaggregations-mixin';
+//<link rel="import" href="../../behaviors/disaggregation-field.html">
+import {ReduxConnectedElement} from '../../ReduxConnectedElement';
+
+/**
+ * @polymer
+ * @customElement
+ * @appliesMixin DisaggregationMixin
+ */
+class DisaggregationField extends DisaggregationMixin(ReduxConnectedElement) {
+  public static get template() {
+    // language=HTML
+    return html`
+     <style>
+      :host {
+        display: block;
+
+        --paper-input-container: {
+          padding: 0;
+        };
+
+        --paper-input-container-input: {
+          font-size: 13px;
+        };
+
+        --paper-input-container-input-webkit-spinner: {
+          display: none;
+        };
+      }
+    </style>
+
+    <paper-input
+        id="field"
+        value="[[value]]"
+        allowed-pattern="^\d*\.?\d*$"
+        invalid="{{invalid}}"
+        validator="[[validator]]"
+        min="[[min]]"
+        on-value-changed="_inputValueChanged"
+        no-label-float
+        required
+        auto-validate>
+    </paper-input>
+    `;
+  }
+
+  @property({type: String})
+  key!: string;
+
+  @property({type: String})
+  coords!: string;
+
+  @property({type: String})
+  validator!: string;
+
+  property({type: Number})
+  min!: number;
+
+  property({type: Number, notify: true})
+  value!: number;
+
+  property({type: Boolean, notify: true})
+  invalid!: boolean;
+
+
+  ready() {
+    this.$.field.validate();
+  };
+
+  attached() {
+    this.$.field.validate();
+    this.fire('register-field', this);
+  };
+
+  detached() {
+  }
+
+  // Cant deregister fields in lifecycle hooks due to:
+  // https://github.com/Polymer/polymer/issues/1159
+  // :( :( :(
+  //
+  // detached: function () {}
+
+  validate() {
+    return this.$.field.validate();
+  };
+
+  getField() {
+    return this.$.field;
+  };
+
+  _inputValueChanged(e: CustomEvent) {
+    var change = {};
+
+    change[this.key] = e.target.value;
+
+    this.fire('field-value-changed', {
+      key: this.coords,
+      value: this._toNumericValues(change),
+    });
+  };
+
+}
+
+window.customElements.define('disaggregation-field', DisaggregationField);
---
+++ polymer_3/src_ts/elements/disaggregations/disaggregation-modal.ts
@@ -0,0 +1,154 @@
+import {html} from '@polymer/polymer';
+import '@polymer/paper-dialog-scrollable/paper-dialog-scrollable';
+import '@polymer/paper-dialog/paper-dialog';
+import '@polymer/iron-flex-layout/iron-flex-layout-classes';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/paper-icon-button/paper-icon-button';
+import '@polymer/paper-button/paper-button';
+import '@unicef-polymer/etools-loading/etools-loading';
+import ModalMixin from '../../mixins/modal-mixin';
+import LocalizeMixin from '../../mixins/localize-mixin';
+import {buttonsStyles} from '../../styles/buttons-styles';
+import {modalStyles} from '../../styles/modal-styles';
+import '../confirm-box';
+import {ReduxConnectedElement} from '../../ReduxConnectedElement';
+import {property} from '@polymer/decorators/lib/decorators';
+
+/**
+ * @polymer
+ * @customElement
+ * @appliesMixin ModalMixin
+ * @appliesMixin LocalizeMixin
+ */
+class DisaggregationModal extends ModalMixin(LocalizeMixin(ReduxConnectedElement)) {
+  public static get template() {
+    // language=HTML
+    return html`
+    ${buttonsStyles}
+    ${modalStyles}
+    <style include="iron-flex iron-flex-alignment iron-flex-reverse">
+      :host {
+        display: block;
+
+        --paper-dialog: {
+          width: 700px;
+
+          & > * {
+            margin: 0;
+          }
+        };
+      }
+
+      ::content disaggregation-table {
+        margin-bottom: 1em;
+      }
+    </style>
+
+    <paper-dialog
+        id="dialog"
+        with-backdrop
+        opened="{{opened}}">
+      <div class="header layout horizontal justified">
+        <h2>[[localize('enter_data')]]</h2>
+
+        <div class="layout horizontal">
+          <p>[[localize('reporting_period')]]: [[reportingPeriod]]</p>
+
+          <paper-icon-button
+              class="self-center"
+              on-tap="close"
+              icon="icons:close">
+          </paper-icon-button>
+        </div>
+      </div>
+
+      <paper-dialog-scrollable>
+        <content select=".meta"></content>
+        <content
+            select="disaggregation-table"
+            class="table">
+        </content>
+      </paper-dialog-scrollable>
+
+      <div class="buttons layout horizontal-reverse">
+        <paper-button
+            class="btn-primary"
+            on-tap="_save"
+            raised>
+          [[localize('save')]]
+        </paper-button>
+
+        <paper-button
+            on-tap="close">
+          [[localize('cancel')]]
+        </paper-button>
+      </div>
+
+      <confirm-box id="confirm"></confirm-box>
+
+      <etools-loading active="[[updatePending]]"></etools-loading>
+    </paper-dialog>
+    `;
+  }
+
+  @property({type: String})
+  reportingPeriod!: string;
+
+  @property({type: Boolean})
+  updatePending = false;
+
+
+  _save() {
+    var table = this.getContentChildNodes('.table')[0];
+    var self = this;
+
+    this.set('updatePending', true);
+
+    table.save()
+      .then(function() {
+        self.set('updatePending', false);
+        self.close();
+      })
+      .catch(function(err) { // jshint ignore:line
+        // TODO: error handling
+
+        self.set('updatePending', false);
+      });
+  },
+
+  _confirm(e: CustomEvent) {
+    e.stopPropagation();
+
+    this.$.confirm.run({
+      body: 'Changing disaggregation will cause your previous data to be lost. ' +
+        'Do you want to continue?',
+      result: e.detail,
+    });
+  },
+
+  _addEventListeners() {
+    this.close = this.close.bind(this);
+    this.addEventListener('dialog.iron-overlay-closed', this.close);
+    this.adjustPosition = this.adjustPosition.bind(this);
+    this.addEventListener('disaggregation-modal-refit', this.adjustPosition);
+    this._confirm = this._confirm.bind(this);
+    this.addEventListener('disaggregation-modal-confirm', this._confirm);
+  },
+
+  _removeEventListeners() {
+    this.removeEventListener('dialog.iron-overlay-closed', this.close);
+    this.removeEventListener('disaggregation-modal-refit', this.adjustPosition);
+    this.removeEventListener('disaggregation-modal-confirm', this._confirm);
+  },
+
+  attached() {
+    this._addEventListeners();
+  },
+
+  detached() {
+    this._removeEventListeners();
+  };
+
+}
+
+window.customElements.define('disaggregation-modal', DisaggregationModal);
