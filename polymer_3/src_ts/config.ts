8d8148547cbb846b72a8977dab61f62b5c544555
dcioara <dan.cioara@nordlogic.com>
2020-03-12 16:11:19 +0200

    Fixes..

--- polymer_3/src_ts/config.ts
+++ polymer_3/src_ts/config.ts
@@ -1,6 +1,29 @@
+const STAGING_DOMAIN = 'staging.unicef';
+const DEV_DOMAIN = 'dev.';
+const DEMO_DOMAIN = 'demo.';
+
+export const isStagingServer = () => {
+  const location = window.location.href;
+  return location.indexOf(STAGING_DOMAIN) > -1;
+};
+
+export const isDevServer = () => {
+  return window.location.href.indexOf(DEV_DOMAIN) > -1;
+};
+
+export const isDemoServer = () => {
+  return window.location.href.indexOf(DEMO_DOMAIN) > -1;
+};
+
 export const getDomainByEnv = () => {
   if (window.location.port === '8081') {
     return 'http://127.0.0.1:8081/app_poly3';
   }
-  return 'https://dev.partnerreportingportal.org';
+  if (isStagingServer()) {
+    return 'https://staging.partnerreportingportal.org/app';
+  }
+  if (isDemoServer()) {
+    return 'https://demo.partnerreportingportal.org/app';
+  }
+  return 'https://dev.partnerreportingportal.org/app';
 };
