663a1b45b18b4ac0b284b7222359dbd9b8149943
Adriana Trif <trif.corina.adriana@gmail.com>
2020-03-16 20:46:12 +0200

    sw config

--- polymer_3/sw-precache-config.js
+++ polymer_3/sw-precache-config.js
@@ -19,7 +19,7 @@ module.exports = {
       handler: 'fastest'
     }
   ],
-  replacePrefix: '/app/',
+  //replacePrefix: '/app/',
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [
     /^\/api\//,
