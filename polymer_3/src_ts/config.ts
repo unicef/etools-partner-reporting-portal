c4227b8130b5fa88d9a0788c3ba735e848959b41
robertavram <robert.avram@gmail.com>
2020-03-27 10:51:49 -0400

    fix Poly3 standalone local setup

--- polymer_3/src_ts/config.ts
+++ polymer_3/src_ts/config.ts
@@ -1,12 +1,5 @@
 export const BASE_PATH = "app";
 
-export const isLocal = () => {
-  return ['9000', '8081'].includes(window.location.port);
-};
-
 export const getDomainByEnv = () => {
-  if (isLocal()) {
-    return 'http://127.0.0.1:' + window.location.port + '/' + BASE_PATH;
-  }
-  return window.location.origin;
+  return document.getElementsByTagName('base')[0].href.slice(0,-1)
 };
