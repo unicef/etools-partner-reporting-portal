3c4c4677e94087d6c2166f5f9074a5bebcffeabc
dcioara <dan.cioara@nordlogic.com>
2020-04-07 16:01:23 +0300

    Updated table links

--- polymer_3/src_ts/elements/list-view-single-indicator.ts
+++ polymer_3/src_ts/elements/list-view-single-indicator.ts
@@ -5,8 +5,8 @@ import '@polymer/iron-flex-layout/iron-flex-layout-classes';
 import '@polymer/paper-tooltip/paper-tooltip';
 import '@polymer/iron-icons/iron-icons';
 import '@polymer/iron-icon/iron-icon';
-import '@polymer/paper-item/paper-item';
-import {PaperItemElement} from '@polymer/paper-item/paper-item';
+import '@polymer/paper-button/paper-button';
+import {PaperButtonElement} from '@polymer/paper-button/paper-button';
 import '@polymer/polymer/lib/elements/dom-if';
 
 import './ip-reporting/ip-reporting-indicator-details';
@@ -68,10 +68,10 @@ class ListViewSingleIndicator extends LocalizeMixin(RoutingMixin(UtilsMixin(Redu
           @apply --link;
         }
 
-        .paper-link{
+        .button-link{
           @apply --link;
           font-size: 13px;
-          cursor: pointer;
+          text-transform: none;
         }
 
         .indicator-progress {
@@ -254,20 +254,20 @@ class ListViewSingleIndicator extends LocalizeMixin(RoutingMixin(UtilsMixin(Redu
 
           <template is="dom-if" if="[[canEdit]]" restamp="true">
             <span class="table-cell table-cell--text table-cell--action self-center">
-              <paper-item class="paper-link" on-tap="_openModal" data-modal-type="edit">[[localize('edit')]]
+              <paper-button class="button-link" on-tap="_openModal" data-modal-type="edit">[[localize('edit')]]
                 <template
                     is="dom-if"
                     if="[[_showLocationsWarning(indicator, type)]]"
                     restamp="true">
                   <iron-icon class="locations-warning" icon="icons:error"></iron-icon>
                 </template>
-              </paper-item>
+              </paper-button>
             </span>
           </template>
 
           <template is="dom-if" if="[[canEditLocations]]" restamp="true">
             <span class="table-cell table-cell--text table-cell--action self-center">
-              <paper-item class="paper-link" on-tap="_openModal" data-modal-type="locations">[[localize('locations')]]</paper-item>
+              <paper-button class="button-link" on-tap="_openModal" data-modal-type="locations">[[localize('locations')]]</paper-button>
             </span>
           </template>
         </div>
@@ -321,7 +321,7 @@ class ListViewSingleIndicator extends LocalizeMixin(RoutingMixin(UtilsMixin(Redu
   }
 
   _openModal(e: CustomEvent) {
-    this.shadowRoot!.querySelector('#modal-' + (e.target as PaperItemElement).dataset.modalType)!.open();
+    this.shadowRoot!.querySelector('#modal-' + (e.target as PaperButtonElement).dataset.modalType)!.open();
   }
 
   _computeIsClusterApp(appName: string) {
