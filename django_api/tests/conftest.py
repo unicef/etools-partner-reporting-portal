cd12b115b88f4b3d39d92c37af8aa901e1d6dea8
Greg Reinbach <greg@reinbach.com>
2020-05-22 12:34:22 -0400

    Call update_notifications in tests configure

--- django_api/tests/conftest.py
+++ django_api/tests/conftest.py
@@ -4,3 +4,9 @@ from django.core.management import call_command
 def pytest_configure(config):
     call_command('loaddata', 'sites', verbosity=0)
     call_command('loaddata', 'reporting_entities', verbosity=0)
+
+
+@pytest.fixture(autouse=True)
+def configure_test(settings, caplog, db):
+    settings.CELERY_ALWAYS_EAGER = True
+    call_command("update_notifications")
