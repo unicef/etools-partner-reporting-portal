0abbf99e5e6899933970a3148a6f0add6c2eb97c
Greg Reinbach <greg@reinbach.com>
2020-10-21 08:38:42 -0400

    Add render_pdf_to_response function
    Wrapper for WeasyPrint library to generate pdf response

---
+++ django_api/django_api/apps/unicef/utils.py
@@ -0,0 +1,29 @@
+import tempfile
+
+from django.http import FileResponse
+from django.template.loader import render_to_string
+
+from weasyprint import CSS, HTML
+from weasyprint.fonts import FontConfiguration
+
+
+def render_pdf_to_response(request, template, data):
+    font_config = FontConfiguration()
+    html_string = render_to_string(f"{template}.html", data)
+    html = HTML(string=html_string)
+    css = CSS(
+        string=render_to_string(f"{template}.css"),
+        font_config=font_config,
+    )
+    result = html.write_pdf(stylesheets=[css], font_config=font_config)
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        response = FileResponse(
+            open(output.name, "rb"),
+            as_attachment=True,
+            filename=f"{template}.pdf",
+        )
+
+    return response
