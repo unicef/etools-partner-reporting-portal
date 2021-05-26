b62046b61165a07eed39281a9631c9402cac1111
dcioara <dan.cioara@nordlogic.com>
2021-05-26 11:36:12 +0300

    adjustments

--- polymer_3/src_ts/redux/selectors/config.ts
+++ polymer_3/src_ts/redux/selectors/config.ts
@@ -1,10 +1,5 @@
 import {RootState} from '../../etools-prp-common/typings/redux.types';
 
-// use instea of App.Selectors.Config
-export const configClusterTypes = function (state: RootState) {
-  return state.config.data.CLUSTER_TYPE_CHOICES;
-};
-
 export const configLoading = function (state: RootState) {
   return state.config.loading;
 };
