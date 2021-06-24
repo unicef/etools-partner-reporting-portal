b577fe5585fb66cf04cf45a9e2bfa396266896fa
Domenico <dom.dinicola@gmail.com>
2021-06-24 14:27:41 -0500

    update composer

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,9 @@ from fabric.operations import local
 def ssh(service):
     """
     ssh into running service container
-    :param service: ['django_api', 'polymer', 'proxy', 'db', 'id-management-frontend']
+    :param service: ['backend', 'polymer', 'proxy', 'db', 'id-management-frontend']
     """
-    assert service in ['django_api', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
+    assert service in ['backend', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
     if service == 'polymer':
         local('docker-compose exec polymer ash')
     else:
@@ -42,9 +42,9 @@ def up_with_bundle(rebuild=True):
 def restart(service):
     """
     restart a service container
-    :param service: ['django_api', 'polymer', 'proxy', 'db', 'id-management-frontend']
+    :param service: ['backend', 'polymer', 'proxy', 'db', 'id-management-frontend']
     """
-    assert service in ['django_api', 'polymer', 'proxy', 'db', 'beater-prp', 'worker-prp', 'id-management-frontend'], "%s is unrecognized service"
+    assert service in ['backend', 'polymer', 'proxy', 'db', 'beater-prp', 'worker-prp', 'id-management-frontend'], "%s is unrecognized service"
     local('docker-compose restart %s' % service)
 
 
@@ -60,7 +60,7 @@ def rebuild(service):
     Re-build docker images for containers.
     """
     if service:
-        assert service in ['django_api', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
+        assert service in ['backend', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
 
     local('docker-compose build %s' % service if service else '')
 
@@ -87,7 +87,7 @@ def stop(service):
         local('docker-compose stop')
 
     elif service:
-        assert service in ['django_api', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
+        assert service in ['backend', 'polymer', 'proxy', 'db', 'id-management-frontend'], "%s is unrecognized service"
         local('docker-compose stop %s' % service)
 
 
@@ -95,60 +95,60 @@ def fixtures():
     """
     Load fixture data for Site and ReportingEntities.
     """
-    local('docker-compose exec django_api python manage.py loaddata sites')
-    local('docker-compose exec django_api python manage.py loaddata reporting_entities')
-    local('docker-compose exec django_api python manage.py loaddata periodic_tasks')
+    local('docker-compose exec backend python manage.py loaddata sites')
+    local('docker-compose exec backend python manage.py loaddata reporting_entities')
+    local('docker-compose exec backend python manage.py loaddata periodic_tasks')
 
 
 def fake_data(quantity=1):
     """
     Load example data from generate_fake_data management command.
     """
-    local('docker-compose exec django_api python manage.py generate_fake_data --quantity %d --clean_before' % (int(quantity)))
+    local('docker-compose exec backend python manage.py generate_fake_data --quantity %d --clean_before' % (int(quantity)))
 
 
 def real_data(area=False):
     """
     Destroy current DB data and uses real sync with PMP API to get all data.
     """
-    local('docker-compose exec django_api python manage.py generate_real_data --clean_before %s' % ("--area %s --fast" % area if area else ""))
+    local('docker-compose exec backend python manage.py generate_real_data --clean_before %s' % ("--area %s --fast" % area if area else ""))
 
 
 def generate_reports():
     """
     Generate reports based on current data
     """
-    local('docker-compose exec django_api python manage.py generate_reports')
+    local('docker-compose exec backend python manage.py generate_reports')
 
 
 def sync_ocha_partners(area=False):
     """
     Generate reports based on current data
     """
-    local('docker-compose exec django_api python manage.py sync_ocha_partners %s' % ("--country %s" % area if area else ""))
+    local('docker-compose exec backend python manage.py sync_ocha_partners %s' % ("--country %s" % area if area else ""))
 
 
 def tests(test_path=''):
     """
-    Run django_api tests.
+    Run backend tests.
     """
-    local('docker-compose exec django_api python manage.py test {} --settings=etools_prp.config.settings.test --parallel --noinput'.format(test_path))
+    local('docker-compose exec backend python manage.py test {} --settings=etools_prp.config.settings.test --parallel --noinput'.format(test_path))
 
 
 def update_real_fixtures(area=False):
     """
     Uses real sync with PMP API to update data.
     """
-    local('docker-compose exec django_api python manage.py generate_real_data %s --update' % ("--area %s --fast" % area if area else ""))
+    local('docker-compose exec backend python manage.py generate_real_data %s --update' % ("--area %s --fast" % area if area else ""))
 
 
 def backend_lint():
     """
     Run python code linter
     """
-    local('docker-compose exec django_api flake8 ./ --count')
+    local('docker-compose exec backend flake8 ./ --count')
 
 
 def preview_uwsgi_log():
-    cmd = 'docker-compose exec django_api tail -f /var/log/uwsgi_global.log'
+    cmd = 'docker-compose exec backend tail -f /var/log/uwsgi_global.log'
     local(cmd)
