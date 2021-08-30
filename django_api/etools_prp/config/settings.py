4f898e4713fb75ad849c9b8d9dc3d04383c6305e
robertavram <robert.avram@gmail.com>
2021-08-30 15:16:41 -0400

    update jwt to lookup on email

--- django_api/etools_prp/config/settings.py
+++ django_api/etools_prp/config/settings.py
@@ -218,8 +218,8 @@ SIMPLE_JWT = {
 
     'AUTH_HEADER_TYPES': ('JWT', ),
     'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-    'USER_ID_FIELD': 'id',
-    'USER_ID_CLAIM': 'user_id',
+    'USER_ID_FIELD': 'email',
+    'USER_ID_CLAIM': 'email',
     'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
 
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
