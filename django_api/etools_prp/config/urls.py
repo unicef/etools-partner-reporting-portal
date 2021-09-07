7496c74fc437e8112b996ccfbbd1d0e04eba9de5
robertavram <robert.avram@gmail.com>
2021-09-07 15:12:02 -0400

    isort

--- django_api/etools_prp/config/urls.py
+++ django_api/etools_prp/config/urls.py
@@ -20,8 +20,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from etools_prp.apps.core.views import HomeView, RedirectAppView, SocialLogoutView, UnauthorizedView
 from rest_framework_swagger.views import get_swagger_view
+
+from etools_prp.apps.core.views import HomeView, RedirectAppView, SocialLogoutView, UnauthorizedView
 
 schema_view = get_swagger_view(title='eTools PRP API')
 
