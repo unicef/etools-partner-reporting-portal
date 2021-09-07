fc8c5a3e293cde36f26093193db348e6175f0f6f
robertavram <robert.avram@gmail.com>
2021-09-07 11:32:08 -0400

    redirect url

--- django_api/etools_prp/apps/core/views.py
+++ django_api/etools_prp/apps/core/views.py
@@ -3,7 +3,9 @@ import importlib
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+from django.views import View
 from django.views.generic import RedirectView, TemplateView
 
 import django_filters
@@ -248,6 +250,21 @@ class HomeView(LoginRequiredMixin, RedirectView):
         else:
             redirect_page = '/unauthorized'
         return redirect_page
+
+
+class RedirectAppView(LoginRequiredMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        url = self.get_redirect_url(request, *args, **kwargs)
+        return HttpResponseRedirect(url)
+
+    def get_redirect_url(self, request, *args, **kwargs):
+        path = request.path
+        if "cluster" in path:
+            new_path = path.replace("app", "cluster", 1)
+        else:
+            new_path = path.replace("app", "ip", 1)
+        return new_path
 
 
 class UnauthorizedView(TemplateView):
