c9f4a9388557eba7108db6601727e3be4684e674
blu <blu@fossa.localdomain>
2022-01-26 15:06:18 +0200

    [ch28213] Swagger deprecated gives error on permissions

--- django_api/etools_prp/apps/indicator/views.py
+++ django_api/etools_prp/apps/indicator/views.py
@@ -309,6 +309,9 @@ class ReportableLocationGoalBaselineInNeedAPIView(ListAPIView, UpdateAPIView):
 
     def check_permissions(self, request):
         super().check_permissions(request)
+        if request.path == '/api/docs/':
+            return
+
         reportable_id = self.kwargs.get('reportable_id')
         reportable = Reportable.objects.get(id=reportable_id)
         or_q_list = [
