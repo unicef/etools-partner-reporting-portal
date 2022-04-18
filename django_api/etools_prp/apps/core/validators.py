247a6a2eb074d80544d92c4f31d7a1d3f6d4526b
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2022-04-18 17:45:19 +0300

    Added JSONSchemaValidator based on jsonschema lib for indicator json fields

--- django_api/etools_prp/apps/core/validators.py
+++ django_api/etools_prp/apps/core/validators.py
@@ -1,14 +1,17 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core import exceptions
+from django.utils.deconstruct import deconstructible
+from django.utils.translation import gettext_lazy as _
 
+from jsonschema import exceptions as jsonschema_exceptions, validate
 from rest_framework.exceptions import ValidationError
-
-from etools_prp.apps.cluster.models import ClusterActivity, ClusterObjective
-from etools_prp.apps.partner.models import PartnerActivity, PartnerActivityProjectContext, PartnerProject
 
 
 class AddIndicatorObjectTypeValidator:
 
     def __call__(self, value):
+        from etools_prp.apps.cluster.models import ClusterActivity, ClusterObjective
+        from etools_prp.apps.partner.models import PartnerActivity, PartnerActivityProjectContext, PartnerProject
         model_choices = {
             ClusterObjective,
             ClusterActivity,
@@ -26,3 +29,28 @@ class AddIndicatorObjectTypeValidator:
 
 
 add_indicator_object_type_validator = AddIndicatorObjectTypeValidator()
+
+
+@deconstructible
+class JSONSchemaValidator:
+    message = _("Invalid JSON: %(value)s")
+    code = 'invalid_json'
+
+    def __init__(self, json_schema, message=None):
+        self.json_schema = json_schema
+        if message:
+            self.message = message
+
+    def __call__(self, value):
+        try:
+            validate(value, self.json_schema)
+        except jsonschema_exceptions.ValidationError as e:
+            raise exceptions.ValidationError(self.message, code=self.code, params={'value': e.message})
+
+    def __eq__(self, other):
+        return (
+                isinstance(other, self.__class__)
+                and (self.json_schema == other.json_schema)
+                and (self.message == other.message)
+                and (self.code == other.code)
+        )
---
+++ django_api/etools_prp/apps/indicator/json_schemas.py
@@ -0,0 +1,22 @@
+
+
+indicator_schema = {
+    "title": "Json schema for total, target, baseline and in_need fields",
+    "type": "object",
+    "additionalProperties": False,
+    "properties": {
+        "c": {"type": "number"},
+        "d": {"type": "number"},
+        "v": {"type": "number"}
+    },
+    "required": ["d", "v"]
+}
+
+disaggregation_schema = {
+    "title": "Disaggregation json schema",
+    "type": "object",
+    "additionalProperties": False,
+    "patternProperties": {
+        "^\((\d*,\s*)*\d*\)$": indicator_schema
+    }
+}
