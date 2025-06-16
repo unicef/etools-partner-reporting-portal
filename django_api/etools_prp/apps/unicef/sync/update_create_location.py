7a7a4f6afe8016b3e3defab2edf46be50e7cada8
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2025-06-16 16:25:20 +0300

    [ch40188]PRP sync: update location workspace and parent

--- django_api/etools_prp/apps/unicef/sync/update_create_location.py
+++ django_api/etools_prp/apps/unicef/sync/update_create_location.py
@@ -1,13 +1,13 @@
 import logging
 
-from etools_prp.apps.core.models import Location
+from etools_prp.apps.core.models import Location, Workspace
 from etools_prp.apps.core.serializers import PMPLocationSerializer
 from etools_prp.apps.unicef.sync.utils import process_model
 
 logger = logging.getLogger(__name__)
 
 
-def update_create_locations(i: dict) -> (list, bool):
+def update_create_locations(i: dict, workspace: Workspace) -> (list, bool):
     locations = list()
     for loc in i['locations']:
         # Create gateway for location
@@ -22,6 +22,10 @@ def update_create_locations(i: dict) -> (list, bool):
             logger.warning("Location code empty! Skipping!")
             return locations, False
 
+        if 'parent_pcode' in loc:
+            parent = Location.objects.filter(p_code=loc['parent_pcode']).first()
+            loc['parent'] = parent.pk if parent else None
+
         # Create location
         location = process_model(
             Location,
@@ -30,9 +34,13 @@ def update_create_locations(i: dict) -> (list, bool):
             {
                 'name': loc['name'],
                 'p_code': loc['p_code'],
-                'admin_level': loc['admin_level'],
+                'admin_level': loc['admin_level']
             }
         )
+
+        if workspace not in location.workspaces.all():
+            location.workspaces.add(workspace)
+
         locations.append(location)
 
     return locations, True
