e8a832b106f416f1dbc39aa68f293fa727889f69
ZakiTech-UNICEF <tfrumuzachi@unicef.org>
2025-06-18 19:12:15 +0300

    fixed url isort

--- django_api/etools_prp/apps/unicef/urls.py
+++ django_api/etools_prp/apps/unicef/urls.py
@@ -1,9 +1,9 @@
 from django.urls import re_path
 
 from .views import (
+    GPDProgressReportAPIView,
     GPDProgressReportAttachmentAPIView,
     GPDProgressReportAttachmentListCreateAPIView,
-    GPDProgressReportAPIView,
     GPDProgressReportDetailsUpdateAPIView,
     GPDProgressReportSRSubmitAPIView,
     GPDProgressReportSubmitAPIView,
