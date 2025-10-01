ae563a5c02548f1cd4b965d0682b9d2cddf2cdba
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2025-10-01 13:29:00 +0300

    fixed progress report has_partner_data check

--- django_api/etools_prp/apps/unicef/models.py
+++ django_api/etools_prp/apps/unicef/models.py
@@ -507,7 +507,8 @@ class ProgressReport(TimeStampedModel):
 
     @cached_property
     def has_partner_data(self):
-        return (any([self.challenges_in_the_reporting_period, self.partner_contribution_to_date,
+        return (any([self.review_overall_status, self.narrative,
+                     self.challenges_in_the_reporting_period, self.partner_contribution_to_date,
                      self.financial_contribution_to_date, self.proposed_way_forward]) or
                 self.attachments.exists() or
                 self.indicator_reports.filter(Q(total__c__gt=0) | Q(total__v__gt=0)).exists())
