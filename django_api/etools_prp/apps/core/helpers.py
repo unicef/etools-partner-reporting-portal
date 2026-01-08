3a8624cca81739cdf007c6ca29c48324c6069ee4
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2026-01-08 16:55:43 +0200

    Admin and cleanup

--- django_api/etools_prp/apps/core/helpers.py
+++ django_api/etools_prp/apps/core/helpers.py
@@ -642,46 +642,7 @@ def create_pr_ir_for_reportable(pd, reportable, pai_ir_for_period, start_date, e
     return indicator_report
 
 
-def update_pr_ir_for_reportable(progress_report, reportable, pai_ir_for_period, start_date, end_date, due_date):
-    from etools_prp.apps.indicator.models import (
-        IndicatorBlueprint,
-        IndicatorLocationData,
-        IndicatorReport,
-        ReportingEntity,
-    )
-
-    logger.info("Updating IndicatorReport for {} - {}".format(start_date, end_date))
-    indicator_report = IndicatorReport.objects.create(
-        progress_report=None,
-        reportable=reportable,
-        parent=pai_ir_for_period,
-        time_period_start=start_date,
-        time_period_end=end_date,
-        due_date=due_date,
-        total={'c': 0, 'd': 0, 'v': 0},
-        overall_status="NoS",
-        report_status="Due",
-        submission_date=None,
-        reporting_entity=ReportingEntity.objects.get(title="UNICEF"),
-    )
-
-    for location_goal in reportable.reportablelocationgoal_set.filter(is_active=True):
-        logger.info("Creating IndicatorReport {} IndicatorLocationData for {} - {}".format(
-            indicator_report, start_date, end_date
-        ))
-        IndicatorLocationData.objects.create(
-            indicator_report=indicator_report,
-            location=location_goal.location,
-            num_disaggregation=indicator_report.disaggregations.count(),
-            level_reported=indicator_report.disaggregations.count(),
-            disaggregation_reported_on=list(indicator_report.disaggregations.values_list(
-                'id', flat=True)),
-            disaggregation={
-                '()': {'c': 0, 'd': 0, 'v': 0}
-            },
-        )
-
-
+@transaction.atomic
 def create_ir_and_ilds_for_pr(pd, reportable_queryset, next_progress_report, start_date, end_date, due_date):
     """
     Create a set of new IndicatorReports and IndicatorLocationData instances per
