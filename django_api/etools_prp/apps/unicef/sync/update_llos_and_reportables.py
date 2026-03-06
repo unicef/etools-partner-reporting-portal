e8d48defdf1624aea3c7a2fa65fc2aafd39f8905
Ema Ciupe <emanuela.ciupe@nordlogic.com>
2026-03-06 18:11:42 +0200

    Accepted Reportable and ReportableLocationGoal will be not deactivated

--- django_api/etools_prp/apps/unicef/sync/update_llos_and_reportables.py
+++ django_api/etools_prp/apps/unicef/sync/update_llos_and_reportables.py
@@ -8,7 +8,10 @@ def update_llos_and_reportables(pd: ProgrammeDocument):
     # Mark all LLO/reportables assigned to this PD as inactive
     llos.update(active=False)
 
-    Reportable.objects.filter(lower_level_outputs__in=llos).update(active=False)
+    syncable_reports = pd.progress_reports.exclude(status='Acc')
+
+    _to_inactivate = Reportable.objects.filter(lower_level_outputs__in=llos, indicator_reports__progress_report__in=syncable_reports, active=True).distinct()
+    _to_inactivate.update(active=False)
 
     # Mark all ReportableLocationGoal instances referred in LLO Reportables as inactive
-    ReportableLocationGoal.objects.filter(reportable__lower_level_outputs__in=llos).update(is_active=False)
+    ReportableLocationGoal.objects.filter(is_active=True, reportable__in=_to_inactivate).update(is_active=False)
